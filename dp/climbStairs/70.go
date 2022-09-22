d2f377427b7174081bb937d4f4e2366a39ab8e2b
Derek <237026504@163.com>
2022-09-22 23:36:42 +0800

    update: repeat clibStairs

--- dp/climbStairs/70.go
+++ dp/climbStairs/70.go
@@ -27,3 +27,13 @@ func get(n int) int {
 	}
 	return dp[n]
 }
+
+func four(n int) int {
+	dp := make([]int, n+1)
+	dp[0] = 1
+	dp[1] = 1
+	for i := 2; i <= n; i++ {
+		dp[i] = dp[i-1] + dp[i-2]
+	}
+	return dp[n]
+}
