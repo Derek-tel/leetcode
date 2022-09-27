549ca694485f6e157e2ee55d543cf48da71974e5
Derek <237026504@163.com>
2022-09-28 01:19:56 +0800

    update: num squares

--- dp/numSquares/279.go
+++ dp/numSquares/279.go
@@ -53,6 +53,18 @@ func get(n int) int {
 	return dp[n]
 }
 
+func five(n int) int {
+	dp := make([]int, n+1)
+	dp[0] = 0
+	for i := 0; i <= n; i++ {
+		dp[i] = i
+		for j := 0; j*j <= i; j++ {
+			dp[i] = min(dp[i], dp[i-j*j]+1)
+		}
+	}
+	return dp[n]
+}
+
 func min(i, j int) int {
 	if i > j {
 		return j
