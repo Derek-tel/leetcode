6eaf8d0fd68cf315a17260ab84231cce92e542c0
Derek <237026504@163.com>
2022-09-30 01:11:23 +0800

    update: word break

--- dp/wordBreak/139.go
+++ dp/wordBreak/139.go
@@ -73,3 +73,22 @@ func get(s string, wordDict []string) bool {
 	}
 	return dp[len(s)]
 }
+
+func five(s string, wordDict []string) bool {
+	dp := make([]bool, len(s)+1)
+	dictMap := make(map[string]bool)
+	for _, word := range wordDict {
+		dictMap[word] = true
+	}
+
+	dp[0] = true
+	for i := 1; i <= len(s); i++ {
+		for j := 0; j < i; j++ {
+			if dp[j] && dictMap[s[j:i]] {
+				dp[i] = true
+				break
+			}
+		}
+	}
+	return dp[len(s)]
+}
