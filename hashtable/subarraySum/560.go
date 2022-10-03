e9719d4bdf62cc12f00797eeb5bb622bada141eb
Derek <237026504@163.com>
2022-10-04 00:00:38 +0800

    update: sub array sum

--- hashtable/subarraySum/560.go
+++ hashtable/subarraySum/560.go
@@ -45,3 +45,18 @@ func get(nums []int, k int) int {
 	}
 	return count
 }
+
+func four(nums []int, k int) int {
+	count := 0
+	prefix := make(map[int]int)
+	prefix[0] = 1
+	sum := 0
+	for i := 0; i < len(nums); i++ {
+		sum = sum + nums[i]
+		if _, ok := prefix[sum-k]; ok {
+			count += prefix[sum-k]
+		}
+		prefix[sum] += 1
+	}
+	return count
+}
