4debcb83dc252da2ce0ed23e1d411ec70fcde70d
Derek <237026504@163.com>
2022-10-06 01:00:49 +0800

    update :merge two list

--- listedlink/mergeTwoLists/21.go
+++ listedlink/mergeTwoLists/21.go
@@ -48,6 +48,27 @@ func mergeTwoList(list1 *ListNode, list2 *ListNode) *ListNode {
 	return prevHead.Next
 }
 
+func five(list1 *ListNode, list2 *ListNode) *ListNode {
+	preHead := &ListNode{Val: -1}
+	prev := preHead
+	for list1 != nil && list2 != nil {
+		if list1.Val <= list2.Val {
+			prev.Next = list1
+			list1 = list1.Next
+		} else {
+			prev.Next = list2
+			list2 = list2.Next
+		}
+		prev = prev.Next
+	}
+	if list1 == nil {
+		prev.Next = list2
+	} else {
+		prev.Next = list1
+	}
+	return preHead.Next
+}
+
 func test(list1 *ListNode, list2 *ListNode) *ListNode {
 	if list1 == nil {
 		return list2
