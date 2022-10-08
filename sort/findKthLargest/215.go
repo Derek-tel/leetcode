ff108dee8b946742edc0c8ebfbebb5871d38f5b2
Derek <237026504@163.com>
2022-10-09 00:30:48 +0800

    update : find kth largest

--- sort/findKthLargest/215.go
+++ sort/findKthLargest/215.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func findKthLargest(nums []int, k int) int {
 
@@ -105,6 +107,40 @@ func done(nums []int, i int, size int) {
 		nums[i], nums[largest] = nums[largest], nums[i]
 		done(nums, largest, size)
 	}
+}
+
+func four(nums []int, k int) int {
+	var buildUp func([]int)
+	var modify func([]int, int, int)
+	buildUp = func(ints []int) {
+		size := len(ints)
+		for i := size / 2; i >= 0; i-- {
+			modify(ints, i, size)
+		}
+	}
+	modify = func(ints []int, i int, size int) {
+		left := i*2 + 1
+		right := i*2 + 2
+		largest := i
+		if left < size && ints[left] > ints[largest] {
+			largest = left
+		}
+		if right < size && ints[right] > ints[largest] {
+			largest = right
+		}
+		if largest != i {
+			ints[i], ints[largest] = ints[largest], ints[i]
+			modify(ints, largest, size)
+		}
+	}
+	buildUp(nums)
+	size := len(nums)
+	for i := len(nums) - 1; i > len(nums)-k; i-- {
+		nums[0], nums[i] = nums[i], nums[0]
+		size--
+		modify(nums, 0, size)
+	}
+	return nums[0]
 }
 
 func main() {
