55046246f29271f51c39c0c05a1f15def9f6829a
Derek <237026504@163.com>
2022-10-10 00:27:14 +0800

    reconstruct queue

--- sort/reconstructQueue/406.go
+++ sort/reconstructQueue/406.go
@@ -49,6 +49,18 @@ func get(people [][]int) (ans [][]int) {
 	return
 }
 
+func four(people [][]int) [][]int {
+	sort.Slice(people, func(i, j int) bool {
+		return people[i][0] > people[j][0] || (people[i][0] == people[j][0] && people[i][1] < people[j][1])
+	})
+	var result [][]int
+	for i := 0; i < len(people); i++ {
+		index := people[i][1]
+		result = append(result[:index], append([][]int{people[i]}, result[index:]...)...)
+	}
+	return result
+}
+
 func main() {
 
 	test1 := [][]int{
