3aea2d3f086687a3bae7b1031c185c2f19722aa1
Derek <237026504@163.com>
2022-10-12 00:01:44 +0800

    longest valid parentheses

--- stack/longestValidParentheses/32.go
+++ stack/longestValidParentheses/32.go
@@ -53,11 +53,35 @@ func get(s string) int {
 			if len(stack) == 0 {
 				stack = append(stack, i)
 			} else {
-				res = max(res, i-stack[len(stack)-1]-1)
+				res = max(res, i-stack[len(stack)-1])
 			}
 		}
 	}
 	return res
+}
+
+func four(s string) int {
+	stack := []int{-1}
+	result := 0
+	maxInt := func(i, j int) int {
+		if i > j {
+			return i
+		}
+		return j
+	}
+	for i := 0; i < len(s); i++ {
+		if s[i] == '(' {
+			stack = append(stack, i)
+		} else {
+			stack = stack[:len(stack)-1]
+			if len(stack) == 0 {
+				stack = append(stack, i)
+			} else {
+				result = maxInt(result, i-stack[len(stack)-1])
+			}
+		}
+	}
+	return result
 }
 
 func max(i, j int) int {
