5629a6d5ccd17fd0950fa4ae269f11b9fef1db3c
Derek <237026504@163.com>
2022-10-23 23:50:19 +0800

    update : sort colors

--- twoPointer/sortColors/75.go
+++ twoPointer/sortColors/75.go
@@ -65,6 +65,19 @@ func test(nums []int) {
 	}
 }
 
+func four(nums []int) {
+	p0, p2 := 0, len(nums)-1
+	for i := 0; i <= p2; i++ {
+		for ; i <= p2 && nums[i] == 2; p2-- {
+			nums[i], nums[p2] = nums[p2], nums[i]
+		}
+		if nums[i] == 0 {
+			nums[i], nums[p0] = nums[p0], nums[i]
+			p0++
+		}
+	}
+}
+
 func main() {
 	test := []int{2, 0, 2, 1, 1, 0}
 	sortColor(test)
