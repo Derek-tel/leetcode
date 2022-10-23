6b3534e1b4ca2c6b36ecebeb6ae946984cb6b998
Derek <237026504@163.com>
2022-10-23 22:20:29 +0800

    post order tarversal

---
+++ tree/postorderTraversal/145.go
@@ -0,0 +1,29 @@
+package postorderTraversal
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func postorderTraversal(root *TreeNode) []int {
+	stack := []*TreeNode{}
+	var result []int
+	var prev *TreeNode
+	for len(stack) > 0 || root != nil {
+		for root != nil {
+			stack = append(stack, root)
+			root = root.Left
+		}
+		top := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		if top.Right == nil || top.Right == prev {
+			result = append(result, top.Val)
+			prev = top
+		} else {
+			stack = append(stack, top)
+			root = top.Right
+		}
+	}
+	return result
+}
