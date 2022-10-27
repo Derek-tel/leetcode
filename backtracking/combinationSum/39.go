247355f428993b125d2d5770523c0bbc84ccbf66
Derek <237026504@163.com>
2022-10-28 00:50:26 +0800

    update : combination sum

--- backtracking/combinationSum/39.go
+++ backtracking/combinationSum/39.go
@@ -164,6 +164,30 @@ func four(nums []int, target int) [][]int {
 	return result
 }
 
+func five(nums []int, target int) [][]int {
+	var com []int
+	var result [][]int
+	var handler func(int, int)
+	handler = func(index int, tar int) {
+		if index == len(nums) {
+			return
+		}
+		if tar == 0 {
+			result = append(result, append([]int(nil), com...))
+			return
+		}
+		handler(index+1, tar)
+
+		if tar > nums[index] {
+			com = append(com, nums[index])
+			handler(index, tar-nums[index])
+			com = com[:len(com)-1]
+		}
+	}
+	handler(0, target)
+	return result
+}
+
 func main() {
 	test := []int{2, 5, 3}
 	tar := 8
