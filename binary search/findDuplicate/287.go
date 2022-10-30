93c7eab8f51b7ca739a747bd1ff116bd623333fa
Derek <237026504@163.com>
2022-10-30 23:57:28 +0800

    find deplicate

--- binary search/findDuplicate/287.go
+++ binary search/findDuplicate/287.go
@@ -84,6 +84,32 @@ func three(nums []int) int {
 	return ans
 }
 
+func four(nums []int) int {
+	length := len(nums)
+	if length < 1 {
+		return -1
+	}
+	left, right := 1, length-1
+	ans := -1
+	for left <= right {
+		mid := left + (right-left)>>1
+		count := 0
+
+		for _, num := range nums {
+			if num <= mid {
+				count++
+			}
+		}
+		if count <= mid {
+			left = mid + 1
+		} else {
+			right = mid - 1
+			ans = mid
+		}
+	}
+	return ans
+}
+
 func main() {
 	fmt.Println(findDuplicate([]int{1, 1}))
 }
