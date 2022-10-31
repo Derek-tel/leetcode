6f885f97788fbee2359c7c9922c67b98d68c31c5
Derek <237026504@163.com>
2022-11-01 00:01:37 +0800

    udpate : search matrix

--- binary search/searchMatrix/240.go
+++ binary search/searchMatrix/240.go
@@ -72,3 +72,20 @@ func four(matrix [][]int, target int) bool {
 	}
 	return false
 }
+
+func five(matrix [][]int, target int) bool {
+	for _, col := range matrix {
+		low, high := 0, len(col)-1
+		for low <= high {
+			mid := low + (high-low)>>1
+			if col[mid] > target {
+				high = mid - 1
+			} else if col[mid] < target {
+				low = mid + 1
+			} else {
+				return true
+			}
+		}
+	}
+	return false
+}
