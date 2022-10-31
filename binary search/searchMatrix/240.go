5e023551634e7ffddc3c361b00e8a84a52a41b2d
Derek <237026504@163.com>
2022-10-31 23:54:56 +0800

    update : search matrix

--- binary search/searchMatrix/240.go
+++ binary search/searchMatrix/240.go
@@ -57,3 +57,18 @@ func three(matrix [][]int, target int) bool {
 	}
 	return false
 }
+
+func four(matrix [][]int, target int) bool {
+	row, col := len(matrix), len(matrix[0])
+	x, y := 0, col-1
+	for x < row && y >= 0 {
+		if matrix[x][y] == target {
+			return true
+		} else if matrix[x][y] < target {
+			x++
+		} else {
+			y--
+		}
+	}
+	return false
+}
