a2c20b884f0303e467b80450d52180690c930b45
Derek <237026504@163.com>
2022-11-02 23:44:23 +0800

    can jump

--- dp/canJump/55.go
+++ dp/canJump/55.go
@@ -64,6 +64,22 @@ func four(nums []int) bool {
 	return true
 }
 
+func five(nums []int) bool {
+	length := len(nums)
+	if length == 0 {
+		return false
+	}
+	farthest := 0
+	for i := 0; i < length; i++ {
+		if i <= farthest {
+			farthest = max(farthest, i+nums[i])
+		} else {
+			return false
+		}
+	}
+	return true
+}
+
 func max(i, j int) int {
 	if i > j {
 		return i
