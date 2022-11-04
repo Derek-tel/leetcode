d5bea735db1a3026531b1b907170260109088d8f
Derek <237026504@163.com>
2022-11-05 00:42:30 +0800

    update : max profit

--- dp/maxProfit/121.go
+++ dp/maxProfit/121.go
@@ -110,3 +110,20 @@ func six(prices []int) int {
 	}
 	return result
 }
+
+func seven(prices []int) int {
+	if len(prices) == 0 {
+		return 0
+	}
+	var result int
+	min := prices[0]
+	for i := 1; i < len(prices); i++ {
+		if prices[i]-min > result {
+			result = prices[i] - min
+		}
+		if prices[i] < min {
+			min = prices[i]
+		}
+	}
+	return result
+}
