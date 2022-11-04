a8058ff75152910df55b4ee183f5f62ac434d98b
Derek <237026504@163.com>
2022-11-05 00:37:38 +0800

    max product

--- dp/maxProduct/152.go
+++ dp/maxProduct/152.go
@@ -74,6 +74,18 @@ func five(nums []int) int {
 	return result
 }
 
+func six(nums []int) int {
+	maxN, minN, result := nums[0], nums[0], nums[0]
+	for i := 1; i < len(nums); i++ {
+		mm := maxN
+		mn := minN
+		maxN = max(max(nums[i]*mm, nums[i]*mn), nums[i])
+		minN = min(min(nums[i]*mm, nums[i]*mn), nums[i])
+		result = max(result, maxN)
+	}
+	return result
+}
+
 func max(i, j int) int {
 	if i > j {
 		return i
