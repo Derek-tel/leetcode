9ed0c92d9f367ffe6c1a3c7a6b1e7c293a09c593
Derek <237026504@163.com>
2022-11-08 23:46:05 +0800

    update : detect cycle

--- listedlink/detectCycle/142.go
+++ listedlink/detectCycle/142.go
@@ -80,3 +80,19 @@ func four(head *ListNode) *ListNode {
 	}
 	return nil
 }
+
+func five(head *ListNode) *ListNode {
+	fast, slow, p := head, head, head
+	for fast != nil && fast.Next != nil {
+		fast = fast.Next.Next
+		slow = slow.Next
+		if fast == slow {
+			for p != slow {
+				p = p.Next
+				slow = slow.Next
+			}
+			return p
+		}
+	}
+	return nil
+}
