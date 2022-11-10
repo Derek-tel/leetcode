466c9a1ac43278f15522912bae12e1b9f1bd653e
Derek <237026504@163.com>
2022-11-11 00:55:55 +0800

    update: reverse

--- math/reverse/7.go
+++ math/reverse/7.go
@@ -14,3 +14,17 @@ func reverse(x int) int {
 	}
 	return result
 }
+
+func two(x int) int {
+	var result int
+
+	for x != 0 {
+		if result < math.MinInt32/10 || result > math.MaxInt32/10 {
+			return 0
+		}
+		digit := x % 10
+		x = x / 10
+		result = result*10 + digit
+	}
+	return result
+}
