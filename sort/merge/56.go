503459840b4d858bef49dc512246d3a2ae0ccd02
Derek <237026504@163.com>
2022-11-12 00:55:38 +0800

    update: merge

--- sort/merge/56.go
+++ sort/merge/56.go
@@ -138,6 +138,57 @@ func quickSort(intervals [][]int, start, end int) {
 
 }
 
+func five(intervals [][]int) [][]int {
+	fiveQuick(intervals, 0, len(intervals))
+
+	var result [][]int
+	temp := intervals[0]
+	for i := 1; i < len(intervals); i++ {
+		if intervals[i][0] > temp[1] {
+			result = append(result, temp)
+			temp = intervals[i]
+		} else {
+			if temp[1] > intervals[i][1] {
+				continue
+			} else {
+				temp[1] = intervals[i][1]
+			}
+		}
+	}
+	result = append(result, temp)
+	return result
+}
+
+func fiveQuick(intervals [][]int, start int, end int) {
+	temp := intervals[start]
+	pivot := start
+	i, j := start, end
+	for i <= j {
+		for pivot <= j && intervals[j][0] >= temp[0] {
+			j--
+		}
+		if pivot <= j {
+			intervals[j], intervals[pivot] = intervals[pivot], intervals[j]
+			pivot = j
+		}
+		for i <= pivot && intervals[i][0] < temp[0] {
+			i++
+		}
+		if i <= pivot {
+			intervals[i], intervals[pivot] = intervals[pivot], intervals[i]
+			pivot = i
+		}
+	}
+	intervals[pivot] = temp
+	if pivot-start > 1 {
+		fiveQuick(intervals, start, pivot-1)
+	}
+	if end-pivot > 1 {
+		fiveQuick(intervals, pivot+1, end)
+	}
+
+}
+
 func main() {
 	test := [][]int{
 		{2, 6},
