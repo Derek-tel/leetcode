9ed33f9a6103ffc5020df803164d50306fcee1d1
Derek <237026504@163.com>
2022-11-13 22:20:08 +0800

    is valid

--- stack/isValid/20.go
+++ stack/isValid/20.go
@@ -77,6 +77,30 @@ func three(s string) bool {
 	return len(stack) == 0
 }
 
+func four(s string) bool {
+	if len(s)%2 == 1 {
+		return false
+	}
+	pair := map[byte]byte{
+		'}': '{',
+		']': '[',
+		')': '(',
+	}
+	var stack []byte
+	for i := 0; i < len(s); i++ {
+		if tag, ok := pair[s[i]]; ok {
+			if len(stack) == 0 || tag != stack[len(stack)-1] {
+				return false
+			} else {
+				stack = stack[:len(stack)-1]
+			}
+		} else {
+			stack = append(stack, s[i])
+		}
+	}
+	return len(stack) == 0
+}
+
 func main() {
 	test := "()"
 	fmt.Println(isValid(test))
