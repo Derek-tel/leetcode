7dcc96141317fa919709c48a65bd52cb6d9b7fba
Derek <237026504@163.com>
2022-11-21 23:16:42 +0800

    num trees

--- tree/numTrees/96.go
+++ tree/numTrees/96.go
@@ -54,6 +54,17 @@ func four(n int) int {
 	return dp[n]
 }
 
+func six(n int) int {
+	dp := make([]int, n+1)
+	dp[0], dp[1] = 1, 1
+	for i := 2; i <= n; i++ {
+		for j := 1; j <= i; j++ {
+			dp[i] += dp[j-1] * dp[i-j]
+		}
+	}
+	return dp[n]
+}
+
 func five(n int) int {
 	if n == 0 {
 		return 0
