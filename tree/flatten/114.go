1586165c5c0ddc344fd4bef730a4a583559badce
Derek <237026504@163.com>
2022-11-21 22:24:37 +0800

    flatten

--- tree/flatten/114.go
+++ tree/flatten/114.go
@@ -93,3 +93,27 @@ func four(root *TreeNode) {
 	}
 	return
 }
+
+func five(root *TreeNode) {
+	if root == nil {
+		return
+	}
+	var handler func(*TreeNode)
+	var list []int
+	handler = func(node *TreeNode) {
+		if node == nil {
+			return
+		}
+		list = append(list, node.Val)
+		handler(node.Left)
+		handler(node.Right)
+	}
+	handler(root)
+	cur := root
+	for i := 1; i < len(list); i++ {
+		cur.Left = nil
+		cur.Right = &TreeNode{list[i], nil, nil}
+		cur = cur.Right
+	}
+	return
+}
