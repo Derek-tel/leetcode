41b296614fe379f823a6db49c923d1a2a3ff9826
Derek <237026504@163.com>
2022-11-21 22:39:56 +0800

    is same tree

--- tree/isSameTree/100.go
+++ tree/isSameTree/100.go
@@ -45,3 +45,13 @@ func four(p *TreeNode, q *TreeNode) bool {
 	}
 	return p.Val == q.Val && four(p.Left, q.Left) && four(p.Right, q.Right)
 }
+
+func five(p *TreeNode, q *TreeNode) bool {
+	if p == nil && q == nil {
+		return true
+	}
+	if p == nil || q == nil {
+		return false
+	}
+	return p.Val == q.Val && five(p.Left, q.Left) && five(p.Right, q.Right)
+}
