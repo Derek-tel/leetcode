3f6600f4a3a8a22fe151a6d1c714597f56bb8ffa
Derek <237026504@163.com>
2022-11-23 23:53:58 +0800

    update: longest common prefix

---
+++ binary search/longestCommonPrefix/14.go
@@ -0,0 +1,29 @@
+package longestCommonPrefix
+
+func longestCommonPrefix(strs []string) string {
+	if len(strs) == 0 {
+		return ""
+	}
+	var lcp func(int, int) string
+	lcp = func(start int, end int) string {
+		if start == end {
+			return strs[start]
+		}
+		mid := (start + end) / 2
+		left, right := lcp(start, mid), lcp(mid+1, end)
+		minLength := min(len(left), len(right))
+		for i := 0; i < minLength; i++ {
+			if left[i] != right[i] {
+				return left[:i]
+			}
+		}
+		return left[:minLength]
+	}
+	return lcp(0, len(strs)-1)
+}
+func min(i, j int) int {
+	if i < j {
+		return i
+	}
+	return j
+}
