93e309a795483926ca1426fb18fc614f18964c03
Derek <237026504@163.com>
2022-11-23 23:22:00 +0800

    remove element

---
+++ twoPointer/removeElement/27.go
@@ -0,0 +1,13 @@
+package removeElement
+
+func removeElement(nums []int, val int) int {
+	flag := 0
+
+	for i := 0; i < len(nums); i++ {
+		if nums[i] != val {
+			nums[flag], nums[i] = nums[i], nums[flag]
+			flag++
+		}
+	}
+	return flag
+}
