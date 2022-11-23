7ab0993d9f4dbfc9c43e9fd16f64b8463ba8c64c
Derek <237026504@163.com>
2022-11-23 22:56:01 +0800

    update: my sqrt

---
+++ math/mySqrt/69.go
@@ -0,0 +1,16 @@
+package mySqrt
+
+func mySqrt(x int) int {
+	left, right := 0, x
+	resp := -1
+	for left <= right {
+		mid := left + (right-left)>>1
+		if mid*mid <= x {
+			resp = mid
+			left = mid + 1
+		} else {
+			right = mid - 1
+		}
+	}
+	return resp
+}
