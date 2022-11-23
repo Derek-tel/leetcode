55e8eae77507ec6dadacc8d89ac8f62e95b0eda7
Derek <237026504@163.com>
2022-11-23 23:05:47 +0800

    update: delete duplicate

---
+++ listedlink/deleteDuplicates/83.go
@@ -0,0 +1,21 @@
+package deleteDuplicates
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func deleteDuplicates(head *ListNode) *ListNode {
+	if head == nil {
+		return head
+	}
+	cur := head
+	for cur.Next != nil {
+		if cur.Val == cur.Next.Val {
+			cur.Next = cur.Next.Next
+		} else {
+			cur = cur.Next
+		}
+	}
+	return head
+}
