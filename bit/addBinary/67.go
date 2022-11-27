554bda3fe1af2f78b859f6a202beac8ab08a7ad6
Derek <237026504@163.com>
2022-11-27 22:03:03 +0800

    update: add binara

--- bit/addBinary/67.go
+++ bit/addBinary/67.go
@@ -25,6 +25,27 @@ func addBinary(a string, b string) string {
 	return resp
 }
 
+func two(a string, b string) string {
+	resp := ""
+	carry := 0
+	lengthA, lengthB := len(a), len(b)
+	n := max(lengthA, lengthB)
+	for i := 0; i < n; i++ {
+		if i < lengthA {
+			carry += int(a[lengthA-i-1] - '0')
+		}
+		if i < lengthB {
+			carry += int(b[lengthB-i-1] - '0')
+		}
+		resp = strconv.Itoa(carry%2) + resp
+		carry = carry / 2
+	}
+	if carry > 0 {
+		resp = "1" + resp
+	}
+	return resp
+}
+
 func max(i, j int) int {
 	if i > j {
 		return i
