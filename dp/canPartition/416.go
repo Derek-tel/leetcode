33de5c2d7257ecbb3480b140fc2f4b9f05cfcaa8
Derek <237026504@163.com>
2022-11-27 22:29:08 +0800

    can partition

--- dp/canPartition/416.go
+++ dp/canPartition/416.go
@@ -137,6 +137,32 @@ func six(nums []int) bool {
 	return dp[half]
 }
 
+func seven(nums []int) bool {
+	count := 0
+	max := 0
+	for i := 0; i < len(nums); i++ {
+		count = count + nums[i]
+		if nums[i] > max {
+			max = nums[i]
+		}
+	}
+	if count%2 == 1 {
+		return false
+	}
+	half := count / 2
+	if half < max {
+		return false
+	}
+	dp := make([]bool, half+1)
+	dp[0] = true
+	for _, num := range nums {
+		for j := half; j >= num; j-- {
+			dp[j] = dp[j] || dp[j-num]
+		}
+	}
+	return dp[half]
+}
+
 func main() {
 	fmt.Println(canPartition([]int{3, 3, 3, 4, 5}))
 }
