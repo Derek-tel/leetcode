babed01f2f7d91b4ba2c2dc167574f00ef2cd22d
Derek <237026504@163.com>
2023-05-28 18:26:25 +0800

    three sum

--- array/threesum/15.go
+++ array/threesum/15.go
@@ -161,6 +161,66 @@ func five(nums []int) [][]int {
 	return result
 }
 
+func six(nums []int) [][]int {
+	sort.Ints(nums)
+	length := len(nums)
+	var result [][]int
+	for i := 1; i < length; i++ {
+		start, end := 0, length-1
+		if i > 1 && nums[i] == nums[i-1] {
+			start = i - 1
+		}
+		for start < i && end > i {
+			if start > 0 && nums[start] == nums[start-1] {
+				start++
+				continue
+			}
+			if end < length-1 && nums[end] == nums[end+1] {
+				end--
+				continue
+			}
+			if sum := nums[start] + nums[i] + nums[end]; sum == 0 {
+				result = append(result, []int{nums[start], nums[i], nums[end]})
+				start++
+				end--
+			} else if sum > 0 {
+				end--
+			} else {
+				start++
+			}
+		}
+	}
+	return result
+}
+
+func seven(nums []int) [][]int {
+	sort.Ints(nums)
+	length := len(nums)
+	var result [][]int
+	for i := 0; i < length-2 && nums[i]+nums[i+1]+nums[i+2] <= 0; i++ {
+		if i > 0 && nums[i] == nums[i-1] || nums[i]+nums[length-1]+nums[length-2] < 0 {
+			continue
+		}
+		for left, right := i+1, length-1; left < right; {
+			total := nums[i] + nums[left] + nums[right]
+			if total == 0 {
+				result = append(result, []int{nums[i], nums[left], nums[right]})
+				for left++; left < right && nums[left] == nums[left-1]; left++ {
+					continue
+				}
+				for right--; left < right && nums[right] == nums[right+1]; right-- {
+					continue
+				}
+			} else if total > 0 {
+				right--
+			} else {
+				left++
+			}
+		}
+	}
+	return result
+}
+
 func main() {
 	test := []int{0, 0, 0, 0, 0}
 	result := threeSum(test)
