738320ff72704478595e9d02ade37ac628b88284
Derek <237026504@163.com>
2023-06-08 00:01:26 +0800

    search matrix

--- binary search/searchMatrix/240.go
+++ binary search/searchMatrix/240.go
@@ -104,3 +104,35 @@ func six(matrix [][]int, target int) bool {
 	}
 	return false
 }
+
+func seven(matrix [][]int, target int) bool {
+	for _, col := range matrix {
+		low, high := 0, len(col)-1
+		for low <= high {
+			mid := low + (high-low)>>1
+			if col[mid] > target {
+				high = mid - 1
+			} else if col[mid] < target {
+				low = mid + 1
+			} else {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func eight(matrix [][]int, target int) bool {
+	row, col := len(matrix), len(matrix[0])
+	x, y := 0, col-1
+	for x < row && y >= 0 {
+		if matrix[x][y] == target {
+			return true
+		} else if matrix[x][y] < target {
+			x++
+		} else {
+			y--
+		}
+	}
+	return false
+}
