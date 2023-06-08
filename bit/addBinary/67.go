bf173c51648576bd29e266554cfc20e2d26d8f2b
Derek <237026504@163.com>
2023-06-08 20:21:54 +0800

    update

--- bit/addBinary/67.go
+++ bit/addBinary/67.go
@@ -46,6 +46,27 @@ func two(a string, b string) string {
 	return resp
 }
 
+func three(a string, b string) string {
+	resp := ""
+	carry := 0
+	lengthA, lengthB := len(a), len(b)
+	n := max(lengthA, lengthB)
+	for i := 0; i < n; i++ {
+		if i < lengthA {
+			carry = carry + int(a[lengthA-i-1]-'0')
+		}
+		if i < lengthB {
+			carry = carry + int(b[lengthB-i-1]-'0')
+		}
+		resp = strconv.Itoa(carry%2) + resp
+		carry = carry / 2
+	}
+	if carry > 0 {
+		resp = "1" + resp
+	}
+	return resp
+}
+
 func max(i, j int) int {
 	if i > j {
 		return i
