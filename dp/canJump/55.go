6cad0b70b31e8662513434dba559c0187387adcc
Derek <237026504@163.com>
2023-06-09 21:11:01 +0800

    can jump

--- dp/canJump/55.go
+++ dp/canJump/55.go
@@ -96,6 +96,21 @@ func six(nums []int) bool {
 	return true
 }
 
+func seven(nums []int) bool {
+	if len(nums) == 0 {
+		return false
+	}
+	farthest := 0
+	for i := 0; i < len(nums); i++ {
+		if i <= farthest {
+			farthest = max(farthest, i+nums[i])
+		} else {
+			return false
+		}
+	}
+	return true
+}
+
 func max(i, j int) int {
 	if i > j {
 		return i
