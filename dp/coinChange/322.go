7cc707d66ed2bda78b78748ecad9f33e0c134823
Derek <237026504@163.com>
2023-06-09 23:53:36 +0800

    update

--- dp/coinChange/322.go
+++ dp/coinChange/322.go
@@ -131,6 +131,23 @@ func seven(coins []int, amount int) int {
 	return dp[amount]
 }
 
+func eight(coins []int, amount int) int {
+	dp := make([]int, amount+1)
+	dp[0] = 0
+	for i := 1; i <= amount; i++ {
+		dp[i] = amount + 1
+		for _, coin := range coins {
+			if i >= coin {
+				dp[i] = min(dp[i], dp[i-coin]+1)
+			}
+		}
+	}
+	if dp[amount] > amount {
+		return -1
+	}
+	return dp[amount]
+}
+
 func main() {
 	a := []int{1, 2, 5}
 	amount := 1
