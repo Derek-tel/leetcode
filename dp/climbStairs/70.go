60317d6418f3011a219f4dac282e0fd070781ae3
Derek <237026504@163.com>
2023-06-09 22:12:40 +0800

    climb stairs

--- dp/climbStairs/70.go
+++ dp/climbStairs/70.go
@@ -47,3 +47,13 @@ func five(n int) int {
 	}
 	return dp[n]
 }
+
+func six(n int) int {
+	dp := make([]int, n+1)
+	dp[0] = 1
+	dp[1] = 1
+	for i := 2; i <= n; i++ {
+		dp[i] = dp[i-1] + dp[i-2]
+	}
+	return dp[n]
+}
