68e041a6cb215187641a119c4019f42d694889ab
Derek <237026504@163.com>
2023-06-13 20:24:38 +0800

    get intersection nod

--- listedlink/getIntersectionNode/160.go
+++ listedlink/getIntersectionNode/160.go
@@ -107,3 +107,23 @@ func five(headA, headB *ListNode) *ListNode {
 	}
 	return pa
 }
+
+func six(headA, headB *ListNode) *ListNode {
+	pa, pb := headA, headB
+	if pa == nil || pb == nil {
+		return nil
+	}
+	for pa != pb {
+		if pa == nil {
+			pa = headB
+		} else {
+			pa = pa.Next
+		}
+		if pb == nil {
+			pb = headA
+		} else {
+			pb = pb.Next
+		}
+	}
+	return pa
+}
