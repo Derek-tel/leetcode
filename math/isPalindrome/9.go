2fb277b0ba613ca9111e271a47dc4a886ef54ead
Derek <237026504@163.com>
2023-06-16 19:31:07 +0800

    is palindrome

--- math/isPalindrome/9.go
+++ math/isPalindrome/9.go
@@ -1,4 +1,6 @@
-package isPalindrome
+package main
+
+import "fmt"
 
 func isPalindrome(x int) bool {
 	if x < 0 || (x%10 == 0 && x != 0) {
@@ -10,4 +12,22 @@ func isPalindrome(x int) bool {
 		x = x / 10
 	}
 	return x == flag || x == flag/10
+}
+
+func one(x int) bool {
+	if x < 0 || (x%10 == 0 && x != 0) {
+		return false
+	}
+	flag := 0
+	for x > flag {
+		flag = flag*10 + x%10
+		x = x / 10
+	}
+	fmt.Println(x, flag)
+	return x == flag || x == flag/10
+}
+
+func main() {
+	x := 10101
+	fmt.Println(one(x))
 }
