416f59ee78adf8eb8cacba5df5bffdddacf8578d
Derek <237026504@163.com>
2023-06-18 00:18:44 +0800

    merge sort

--- sort/mergeSort.go
+++ sort/mergeSort.go
@@ -188,9 +188,42 @@ func five(nums []int) []int {
 	return handler(five(left), five(right))
 }
 
+func six(nums []int) []int {
+	var handler func([]int, []int) []int
+	handler = func(left []int, right []int) []int {
+		var temp []int
+		i, j := 0, 0
+		for {
+			if i >= len(left) {
+				temp = append(temp, right[j:]...)
+				break
+			}
+			if j >= len(right) {
+				temp = append(temp, left[i:]...)
+				break
+			}
+			if left[i] < right[j] {
+				temp = append(temp, left[i])
+				i++
+			} else {
+				temp = append(temp, right[j])
+				j++
+			}
+		}
+		return temp
+	}
+	if len(nums) <= 1 {
+		return nums
+	}
+	mid := len(nums) / 2
+	left := nums[0:mid]
+	right := nums[mid:]
+	return handler(six(left), six(right))
+}
+
 // 测试：
 func main() {
 	arr := []int{0, 1, 5, 2, 3, 8, 0, 4, 9, 2}
-	r := four(arr)
+	r := six(arr)
 	fmt.Println(r)
 }
