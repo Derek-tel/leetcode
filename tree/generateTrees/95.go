f233e2a2428dff235007ce86550a07bac871a242
Derek <237026504@163.com>
2023-06-20 22:26:31 +0800

    generate trees

--- tree/generateTrees/95.go
+++ tree/generateTrees/95.go
@@ -135,3 +135,28 @@ func five(n int) []*TreeNode {
 	}
 	return handler(1, n)
 }
+
+func six(n int) []*TreeNode {
+	if n == 0 {
+		return []*TreeNode{}
+	}
+	var handler func(int, int) []*TreeNode
+	handler = func(start int, end int) []*TreeNode {
+		tree := []*TreeNode{}
+		if start > end {
+			return append(tree, nil)
+		}
+		for i := start; i <= end; i++ {
+			left := handler(start, i-1)
+			right := handler(i+1, end)
+			for _, l := range left {
+				for _, r := range right {
+					flag := &TreeNode{i, l, r}
+					tree = append(tree, flag)
+				}
+			}
+		}
+		return tree
+	}
+	return handler(1, n)
+}
