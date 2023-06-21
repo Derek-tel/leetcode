62a1759f446cdca4b6896845f3b7c6b03281e5ff
Derek <237026504@163.com>
2023-06-21 20:27:45 +0800

    is symmetric

--- tree/isSymmetric/101.go
+++ tree/isSymmetric/101.go
@@ -105,3 +105,18 @@ func six(root *TreeNode) bool {
 
 	return handler(root.Left, root.Right)
 }
+
+func seven(root *TreeNode) bool {
+	var handler func(*TreeNode, *TreeNode) bool
+	handler = func(left *TreeNode, right *TreeNode) bool {
+		if left == nil && right == nil {
+			return true
+		}
+		if left == nil || right == nil {
+			return false
+		}
+		return left.Val == right.Val && handler(left.Left, right.Right) && handler(left.Right, right.Left)
+	}
+
+	return handler(root.Left, root.Right)
+}
