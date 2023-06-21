65ed2d97dba49b1515d9fb183044238c92b599ac
Derek <237026504@163.com>
2023-06-21 22:27:18 +0800

    num trees

--- tree/numTrees/96.go
+++ tree/numTrees/96.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type TreeNode struct {
 	Val   int
@@ -88,6 +90,43 @@ func five(n int) int {
 		return trees
 	}
 	return len(handler(1, n))
+}
+
+func seven(n int) int {
+	if n == 0 {
+		return 0
+	}
+	var handler func(int, int) []*TreeNode
+	handler = func(start int, end int) []*TreeNode {
+		var trees []*TreeNode
+		if start > end {
+			return append(trees, nil)
+		}
+		for i := start; i <= end; i++ {
+			left := handler(start, i-1)
+			right := handler(i+1, end)
+			for _, l := range left {
+				for _, r := range right {
+					tree := &TreeNode{i, l, r}
+					trees = append(trees, tree)
+				}
+			}
+		}
+		return trees
+	}
+	return len(handler(1, n))
+
+}
+
+func nine(n int) int {
+	dp := make([]int, n+1)
+	dp[0], dp[1] = 1, 1
+	for i := 2; i <= n; i++ {
+		for j := 1; j <= i; j++ {
+			dp[i] += dp[j-1] * dp[i-j]
+		}
+	}
+	return dp[n]
 }
 
 func main() {
