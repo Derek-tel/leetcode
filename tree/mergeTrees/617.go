1bd692bdd3d959e0123ab4af1bc1e29071dc40e0
Derek <237026504@163.com>
2023-06-21 22:15:49 +0800

    merge trees

--- tree/mergeTrees/617.go
+++ tree/mergeTrees/617.go
@@ -78,3 +78,16 @@ func five(root1 *TreeNode, root2 *TreeNode) *TreeNode {
 	root1.Right = five(root1.Right, root2.Right)
 	return root1
 }
+
+func six(root1 *TreeNode, root2 *TreeNode) *TreeNode {
+	if root1 == nil {
+		return root2
+	}
+	if root2 == nil {
+		return root1
+	}
+	root1.Val = root1.Val + root2.Val
+	root1.Left = six(root1.Left, root2.Left)
+	root1.Right = six(root1.Right, root2.Right)
+	return root1
+}
