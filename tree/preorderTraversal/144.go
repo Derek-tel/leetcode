cb1c4beac02a556ddceb4d38a6cb80981c5ed4bb
Derek <237026504@163.com>
2023-06-23 15:58:02 +0800

    pre order traversal

--- tree/preorderTraversal/144.go
+++ tree/preorderTraversal/144.go
@@ -85,3 +85,19 @@ func five(root *TreeNode) []int {
 	}
 	return result
 }
+
+func six(root *TreeNode) []int {
+	var stack []*TreeNode
+	var result []int
+	for len(stack) > 0 || root != nil {
+		for root != nil {
+			result = append(result, root.Val)
+			stack = append(stack, root)
+			root = root.Left
+		}
+		top := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		root = top.Right
+	}
+	return result
+}
