c3a496bdee8d27991d984367db4d0599b570c97a
Derek <237026504@163.com>
2023-06-23 18:03:05 +0800

    remove duplicate

--- twoPointer/removeDuplicates/26.go
+++ twoPointer/removeDuplicates/26.go
@@ -14,3 +14,18 @@ func removeDuplicates(nums []int) int {
 	}
 	return slow
 }
+
+func first(nums []int) int {
+	n := len(nums)
+	if n == 0 {
+		return 0
+	}
+	slow := 1
+	for fast := 1; fast < n; fast++ {
+		if nums[fast] != nums[fast-1] {
+			nums[slow] = nums[fast]
+			slow++
+		}
+	}
+	return slow
+}
