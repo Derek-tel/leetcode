35300607a6b5310092484756dd3699512ca4c17a
Derek <237026504@163.com>
2023-06-23 17:59:30 +0800

    move zeroes

--- twoPointer/moveZeroes/283.go
+++ twoPointer/moveZeroes/283.go
@@ -66,3 +66,14 @@ func five(nums []int) []int {
 	}
 	return nums
 }
+
+func six(nums []int) []int {
+	flag := 0
+	for i := 0; i < len(nums); i++ {
+		if nums[i] != 0 {
+			nums[i], nums[flag] = nums[flag], nums[i]
+			flag++
+		}
+	}
+	return nums
+}
