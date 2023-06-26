2dfd29235cf2b59d7d4d1c3cf59c85fa06fae519
Derek <237026504@163.com>
2023-06-26 19:53:13 +0800

    longest common prefix

--- binary search/longestCommonPrefix/14.go
+++ binary search/longestCommonPrefix/14.go
@@ -43,12 +43,6 @@ func two(strs []string) string {
 	}
 	return lcp(0, len(strs)-1)
 }
-func min(i, j int) int {
-	if i < j {
-		return i
-	}
-	return j
-}
 
 func three(strs []string) string {
 	if len(strs) == 0 {
@@ -70,4 +64,33 @@ func three(strs []string) string {
 		return left[:minLength]
 	}
 	return lcp(0, len(strs)-1)
+}
+
+func four(strs []string) string {
+	if len(strs) == 0 {
+		return ""
+	}
+	var lcp func(int, int) string
+	lcp = func(start int, end int) string {
+		if start == end {
+			return strs[start]
+		}
+		mid := (start + end) / 2
+		left, right := lcp(start, mid), lcp(mid+1, end)
+		minLength := min(len(left), len(right))
+		for i := 0; i < minLength; i++ {
+			if left[i] != right[i] {
+				return left[:i]
+			}
+		}
+		return left[:minLength]
+	}
+	return lcp(0, len(strs)-1)
+}
+
+func min(i, j int) int {
+	if i < j {
+		return i
+	}
+	return j
 }
