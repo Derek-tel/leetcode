6036b02fe201b555c975f0bbdbae63c9b96bdb7c
Derek <237026504@163.com>
2023-06-26 20:09:25 +0800

    search insert

--- binary search/searchInsert/35.go
+++ binary search/searchInsert/35.go
@@ -32,3 +32,19 @@ func one(num []int, target int) int {
 	}
 	return len(num)
 }
+
+func two(nums []int, target int) int {
+	left, right := 0, len(nums)-1
+	for left <= right {
+		mid := left + (right-left)>>1
+		if nums[mid] >= target {
+			if mid == 0 || nums[mid-1] < target {
+				return mid
+			}
+			right = mid - 1
+		} else {
+			left = left + 1
+		}
+	}
+	return len(nums)
+}
