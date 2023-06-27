36c0ec414fe66ea13c9950f7eb5c6bc171e4faa4
Derek <237026504@163.com>
2023-06-27 20:28:13 +0800

    53

--- dp/maxSubArray/53.go
+++ dp/maxSubArray/53.go
@@ -133,6 +133,24 @@ func seven(nums []int) int {
 	return result
 }
 
+func eight(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+	result := nums[0]
+	dp := make([]int, len(nums))
+	dp[0] = nums[0]
+	for i := 1; i < len(nums); i++ {
+		if dp[i-1] < 0 {
+			dp[i] = nums[i]
+		} else {
+			dp[i] = dp[i-1] + nums[i]
+		}
+		result = max(result, dp[i])
+	}
+	return result
+}
+
 func max(i, j int) int {
 	if i > j {
 		return i
