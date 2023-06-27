a79114e8f0b7bcf0f3e9ec53a4c0027aec1c2f50
Derek <237026504@163.com>
2023-06-27 20:00:31 +0800

    494

--- dp/findTargetSumWays/494.go
+++ dp/findTargetSumWays/494.go
@@ -170,3 +170,23 @@ func eight(nums []int, target int) int {
 	}
 	return dp[neg]
 }
+
+func nine(nums []int, target int) int {
+	count := 0
+	for _, num := range nums {
+		count += num
+	}
+	diff := count - target
+	if diff < 0 || diff%2 == 1 {
+		return 0
+	}
+	neg := diff / 2
+	dp := make([]int, neg+1)
+	dp[0] = 1
+	for _, num := range nums {
+		for j := neg; j >= num; j-- {
+			dp[j] = dp[j] + dp[j-num]
+		}
+	}
+	return dp[neg]
+}
