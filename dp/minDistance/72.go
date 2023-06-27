cc529fbbc1b405976bfe48147311ebff5f71c4db
Derek <237026504@163.com>
2023-06-27 20:36:10 +0800

    72

--- dp/minDistance/72.go
+++ dp/minDistance/72.go
@@ -209,6 +209,32 @@ func seven(word1, word2 string) int {
 	return dp[lengthA][lengthB]
 }
 
+func eight(word1, word2 string) int {
+	lengthA, lengthB := len(word1), len(word2)
+	dp := make([][]int, lengthA+1)
+	for i := 0; i < len(dp); i++ {
+		dp[i] = make([]int, lengthB+1)
+	}
+	for i := 0; i <= lengthA; i++ {
+		dp[i][0] = i
+	}
+	for j := 0; j <= lengthA; j++ {
+		dp[0][j] = j
+	}
+	for i := 1; i <= lengthA; i++ {
+		for j := 1; j <= lengthB; j++ {
+			left := dp[i-1][j] + 1
+			down := dp[i][j-1] + 1
+			leftDown := dp[i-1][j-1] + 1
+			if word1[i-1] == word2[j-1] {
+				leftDown--
+			}
+			dp[i][j] = min(min(left, down), leftDown)
+		}
+	}
+	return dp[lengthA][lengthB]
+}
+
 func min(i, j int) int {
 	if i < j {
 		return i
