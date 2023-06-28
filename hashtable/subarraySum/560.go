4c5ab0be5de14f05e0019ea1997778a69b900648
Derek <237026504@163.com>
2023-06-28 21:05:58 +0800

    560

--- hashtable/subarraySum/560.go
+++ hashtable/subarraySum/560.go
@@ -90,3 +90,18 @@ func six(nums []int, k int) int {
 	}
 	return count
 }
+
+func seven(nums []int, k int) int {
+	count := 0
+	prefix := make(map[int]int)
+	prefix[0] = 1
+	sum := 0
+	for i := 0; i < len(nums); i++ {
+		sum += nums[i]
+		if _, ok := prefix[sum-k]; ok {
+			count += prefix[sum-k]
+		}
+		prefix[sum] += 1
+	}
+	return count
+}
