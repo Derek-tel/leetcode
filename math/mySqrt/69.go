4eea625c092f4d3b7bbba22742db02d334c88b02
Derek <237026504@163.com>
2023-06-28 21:49:36 +0800

    69

--- math/mySqrt/69.go
+++ math/mySqrt/69.go
@@ -29,3 +29,18 @@ func one(x int) int {
 	}
 	return resp
 }
+
+func two(x int) int {
+	left, right := 0, x
+	resp := -1
+	for left <= right {
+		mid := left + (right-left)>>1
+		if mid*mid <= x {
+			resp = mid
+			left = mid + 1
+		} else {
+			right = mid - 1
+		}
+	}
+	return resp
+}
--- math/reverse/7.go
+++ math/reverse/7.go
@@ -41,3 +41,16 @@ func three(x int) int {
 	}
 	return result
 }
+
+func four(x int) int {
+	var result int
+	for x != 0 {
+		if result < math.MinInt32/10 || result > math.MaxInt32/10 {
+			return 0
+		}
+		digit := x % 10
+		x = x / 10
+		result = result*10 + digit
+	}
+	return result
+}
