0314995575cac4897a045917e3f3dfc41a3a867f
Derek <237026504@163.com>
2023-06-28 20:37:02 +0800

    621

--- hashtable/leastInterval/621.go
+++ hashtable/leastInterval/621.go
@@ -22,12 +22,6 @@ func leastInterval(tasks []byte, n int) int {
 
 	return max(len(tasks), minLength)
 }
-func max(i, j int) int {
-	if i < j {
-		return j
-	}
-	return i
-}
 
 func test(task []byte, n int) int {
 	counter := make([]int, 26)
@@ -107,7 +101,29 @@ func six(task []byte, n int) int {
 		}
 	}
 	return max(len(task), minlength)
+}
 
+func seven(tasks []byte, n int) int {
+	counter := make([]int, 26)
+	for i := 0; i < len(tasks); i++ {
+		counter[tasks[i]-'A']++
+	}
+	sort.Ints(counter)
+	most := counter[len(counter)-1]
+	minLength := (n+1)*(most-1) + 1
+	for i := 0; i < len(counter)-1; i++ {
+		if counter[i] == most {
+			minLength++
+		}
+	}
+	return max(len(tasks), minLength)
+}
+
+func max(i, j int) int {
+	if i < j {
+		return j
+	}
+	return i
 }
 
 func main() {
