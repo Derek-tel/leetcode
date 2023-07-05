783bc302cd148053bf2a0fcb49275382a3ea263b
Derek <237026504@163.com>
2023-07-06 00:38:56 +0800

    98

--- tree/isValidBST/98.go
+++ tree/isValidBST/98.go
@@ -123,3 +123,14 @@ func seven(root *TreeNode) bool {
 	}
 	return handler(root, math.MinInt, math.MaxInt)
 }
+
+func eight(root *TreeNode) bool {
+	var handler func(*TreeNode, int, int) bool
+	handler = func(node *TreeNode, min int, max int) bool {
+		if node == nil {
+			return true
+		}
+		return node.Val > min && node.Val < max && handler(node.Left, min, node.Val) && handler(node.Right, node.Val, max)
+	}
+	return handler(root, math.MinInt, math.MaxInt)
+}
