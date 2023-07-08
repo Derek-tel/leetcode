0a014b3e90c27efe9eead0e844e2234d1d74e7e7
Derek <237026504@163.com>
2023-07-09 00:40:18 +0800

    42

--- twoPointer/trap/42.go
+++ twoPointer/trap/42.go
@@ -146,6 +146,30 @@ func six(height []int) int {
 	return sum
 }
 
+func seven(height []int) int {
+	left, right := 0, len(height)-1
+	leftMax, rightMax := 0, 0
+	sum := 0
+	for left < right {
+		if height[left] < height[right] {
+			if height[left] > leftMax {
+				leftMax = height[left]
+			} else {
+				sum = sum + (leftMax - height[left])
+			}
+			left++
+		} else {
+			if height[right] > rightMax {
+				rightMax = height[right]
+			} else {
+				sum = sum + (rightMax - height[right])
+			}
+			right--
+		}
+	}
+	return sum
+}
+
 func main() {
 	test := []int{0, 1, 0, 2, 1, 0}
 	result := trap(test)
