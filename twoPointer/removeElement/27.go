ddfde7fe3a6e7fc2b4aa6e2bb7bc01e9268e5088
Derek <237026504@163.com>
2023-07-09 00:30:52 +0800

    27

--- twoPointer/removeElement/27.go
+++ twoPointer/removeElement/27.go
@@ -23,3 +23,14 @@ func first(nums []int, val int) int {
 
 	return flag
 }
+
+func two(nums []int, val int) int {
+	flag := 0
+	for i := 0; i < len(nums); i++ {
+		if nums[i] != val {
+			nums[flag], nums[i] = nums[i], nums[flag]
+			flag++
+		}
+	}
+	return flag
+}
