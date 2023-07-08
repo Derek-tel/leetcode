f6fd0d1c022bb98c5a5bdd73e7a183f2aca2f8b8
Derek <237026504@163.com>
2023-07-09 00:29:19 +0800

    26

--- twoPointer/removeDuplicates/26.go
+++ twoPointer/removeDuplicates/26.go
@@ -29,3 +29,18 @@ func first(nums []int) int {
 	}
 	return slow
 }
+
+func two(nums []int) int {
+	n := len(nums)
+	if n == 0 {
+		return 0
+	}
+	slow := 1
+	for fast := 1; fast < n; fast++ {
+		if nums[fast] != nums[fast-1] {
+			nums[slow] = nums[fast]
+			slow++
+		}
+	}
+	return slow
+}
