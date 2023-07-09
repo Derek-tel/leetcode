7f3089f625b130d4edc5f3570ae0a7e11b9fd1f5
Derek <237026504@163.com>
2023-07-09 23:08:06 +0800

    chan

--- test/feibo.go
+++ test/feibo.go
@@ -92,3 +92,25 @@ func mainTest() {
 	}()
 	fibonacci(c, quit)
 }
+
+func fibTestMain() {
+	c := make(chan int)
+	quit := make(chan int)
+	go func() {
+		for i := 0; i < 10; i++ {
+			fmt.Println(<-c)
+		}
+		quit <- 0
+	}()
+
+	x, y := 0, 1
+	for {
+		select {
+		case c <- x:
+			x, y = y, x+y
+		case <-quit:
+			fmt.Println("quit")
+			return
+		}
+	}
+}
