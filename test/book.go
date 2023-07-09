c5d820d12c95262bffc22d581bfef67beb0257e4
Derek <237026504@163.com>
2023-07-09 22:52:22 +0800

    chan

--- test/book.go
+++ test/book.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	chA := make(chan bool, 1)
@@ -21,6 +23,32 @@ func main() {
 		}()
 		for i := 1; i <= 10; i++ {
 			if ok := <-ChB; ok {
+				fmt.Println(i * 2)
+				chA <- true
+			}
+		}
+	}()
+
+	chA <- true
+	<-exit
+}
+
+func one() {
+	chA := make(chan bool, 1)
+	chB := make(chan bool)
+	exit := make(chan bool)
+	go func() {
+		for i := 0; i < 10; i++ {
+			if ok := <-chA; ok {
+				fmt.Println(i*2 + 1)
+				chB <- true
+			}
+		}
+	}()
+	go func() {
+		defer func() { close(exit) }()
+		for i := 1; i <= 10; i++ {
+			if ok := <-chB; ok {
 				fmt.Println(i * 2)
 				chA <- true
 			}
