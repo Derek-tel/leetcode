d2458ad0d0d513267aa2d3db4a9c1253c685fc1f
Derek <237026504@163.com>
2023-07-17 23:58:41 +0800

    42

--- dp/trap/42.go
+++ dp/trap/42.go
@@ -140,6 +140,25 @@ func six(height []int) int {
 	return result
 }
 
+func seven(height []int) int {
+	left := make([]int, len(height))
+	right := make([]int, len(height))
+	left[0] = height[0]
+	for i := 1; i < len(height); i++ {
+		left[i] = max(left[i-1], height[i])
+	}
+	right[len(height)-1] = height[len(height)-1]
+	for i := len(height) - 2; i >= 0; i-- {
+		right[i] = max(right[i+1], height[i])
+	}
+
+	result := 0
+	for i := 0; i < len(height); i++ {
+		result += min(left[i], right[i]) - height[i]
+	}
+	return result
+}
+
 func max(i, j int) int {
 	if i > j {
 		return i
