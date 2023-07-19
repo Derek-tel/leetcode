b625289fa9a070ee6ae852c5013bdaac52dbe927
Derek <237026504@163.com>
2023-07-19 18:29:26 +0800

    169

--- hashtable/majorityElement/169.go
+++ hashtable/majorityElement/169.go
@@ -129,3 +129,21 @@ func seven(nums []int) int {
 	}
 	return p
 }
+
+func eight(nums []int) int {
+	count := 0
+	var p int
+	for _, num := range nums {
+		if count == 0 {
+			p = num
+			count++
+		} else {
+			if p == num {
+				count++
+			} else {
+				count--
+			}
+		}
+	}
+	return p
+}
