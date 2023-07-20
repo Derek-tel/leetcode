881c2191b3cc16af6e77a8873bf5f7cadac35fc2
Derek <237026504@163.com>
2023-07-20 23:39:11 +0800

    83

--- listedlink/deleteDuplicates/83.go
+++ listedlink/deleteDuplicates/83.go
@@ -49,3 +49,18 @@ func two(head *ListNode) *ListNode {
 	}
 	return head
 }
+
+func three(head *ListNode) *ListNode {
+	if head == nil {
+		return nil
+	}
+	cur := head
+	for cur.Next != nil {
+		if cur.Val == cur.Next.Val {
+			cur.Next = cur.Next.Next
+		} else {
+			cur = cur.Next
+		}
+	}
+	return head
+}
