491c86c7615395fe504197eeb652a5af3ffb2892
Derek <237026504@163.com>
2023-07-25 20:41:57 +0800

    155

--- stack/MinStack/155.go
+++ stack/MinStack/155.go
@@ -1,39 +1,45 @@
 package MinStack
 
-import (
-	"math"
-)
+import "math"
 
 type MinStack struct {
-	Stack []int
-	Min   []int
+	stack []int
+	min   []int
 }
 
 func Constructor() MinStack {
 	return MinStack{
-		Stack: []int{},
-		Min:   []int{math.MaxInt},
+		stack: []int{},
+		min:   []int{math.MaxInt},
 	}
 }
 
 func (this *MinStack) Push(val int) {
-	this.Stack = append(this.Stack, val)
-	top := this.Min[len(this.Min)-1]
-	this.Min = append(this.Min, min(top, val))
+	this.stack = append(this.stack, val)
+	this.min = append(this.min, min(val, this.min[len(this.min)-1]))
 }
 
 func (this *MinStack) Pop() {
-	this.Stack = this.Stack[:len(this.Stack)-1]
-	this.Min = this.Min[:len(this.Min)-1]
+	this.stack = this.stack[:len(this.stack)-1]
+	this.min = this.min[:len(this.min)-1]
 }
 
 func (this *MinStack) Top() int {
-	return this.Stack[len(this.Stack)-1]
+	return this.stack[len(this.stack)-1]
 }
 
 func (this *MinStack) GetMin() int {
-	return this.Min[len(this.Min)-1]
+	return this.min[len(this.min)-1]
 }
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.Push(val);
+ * obj.Pop();
+ * param_3 := obj.Top();
+ * param_4 := obj.GetMin();
+ */
 
 func min(x, j int) int {
 	if x < j {
