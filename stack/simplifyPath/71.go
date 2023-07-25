5b73299a748ab962dd3b0234620f4b6adc4a130f
Derek <237026504@163.com>
2023-07-25 20:44:14 +0800

    71

--- stack/simplifyPath/71.go
+++ stack/simplifyPath/71.go
@@ -107,6 +107,20 @@ func seven(path string) string {
 	return "/" + strings.Join(stack, "/")
 }
 
+func eight(path string) string {
+	var stack []string
+	for _, v := range strings.Split(path, "/") {
+		if v == ".." {
+			if len(stack) > 0 {
+				stack = stack[:len(stack)-1]
+			}
+		} else if v != "." && v != "" {
+			stack = append(stack, v)
+		}
+	}
+	return "/" + strings.Join(stack, "/")
+}
+
 func main() {
 	test := "//root/"
 	fmt.Println(strings.Split(test, "/"))
