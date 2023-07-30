170a0bdee6afa6c00493adfabb40c10d08a2d9b9
Derek <237026504@163.com>
2023-07-30 23:58:30 +0800

    617

--- tree/mergeTrees/617.go
+++ tree/mergeTrees/617.go
@@ -104,3 +104,16 @@ func seven(root1 *TreeNode, root2 *TreeNode) *TreeNode {
 	root1.Right = seven(root1.Right, root2.Right)
 	return root1
 }
+
+func eight(root1 *TreeNode, root2 *TreeNode) *TreeNode {
+	if root1 == nil {
+		return root2
+	}
+	if root2 == nil {
+		return root1
+	}
+	root1.Val = root1.Val + root2.Val
+	root1.Left = eight(root1.Left, root2.Left)
+	root1.Right = eight(root1.Right, root2.Right)
+	return root1
+}
