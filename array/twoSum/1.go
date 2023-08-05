c55d31b9d47550a849cec41c35ee063fc1a91a1d
Derek <237026504@163.com>
2023-08-06 00:16:51 +0800

    1

--- array/twoSum/1.go
+++ array/twoSum/1.go
@@ -109,3 +109,15 @@ func nine(nums []int, target int) []int {
 	}
 	return nil
 }
+
+func ten(nums []int, target int) []int {
+	m := make(map[int]int)
+	for i := 0; i < len(nums); i++ {
+		another := target - nums[i]
+		if index, ok := m[another]; ok {
+			return []int{index, i}
+		}
+		m[nums[i]] = i
+	}
+	return nil
+}
