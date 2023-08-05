44177919abc96079bfadb7d7eae3d00f8e36d0c2
Derek <237026504@163.com>
2023-08-05 23:03:42 +0800

    79

--- array/exist/79.go
+++ array/exist/79.go
@@ -376,6 +376,47 @@ func eight(board [][]byte, word []byte) bool {
 	return false
 }
 
+type dirPairNine struct {
+	x, y int
+}
+
+var dirNine = []dirPairNine{{-1, 0}, {1, 0}, {0, 1}, {0, -1}}
+
+func nine(board [][]byte, word string) bool {
+	h, w := len(board), len(board[0])
+	visit := make([][]bool, h)
+	for i := 0; i < h; i++ {
+		visit[i] = make([]bool, w)
+	}
+	var handler func(int, int, int) bool
+	handler = func(index int, x int, y int) bool {
+		if board[x][y] != word[index] {
+			return false
+		}
+		if index == len(word)-1 {
+			return true
+		}
+		visit[x][y] = true
+		defer func() { visit[x][y] = false }()
+		for _, v := range dirNine {
+			if newX, newY := x+v.x, y+v.y; newX >= 0 && newX < h && newY >= 0 && newY < w && visit[newX][newY] == false {
+				if handler(index+1, newX, newY) {
+					return true
+				}
+			}
+		}
+		return false
+	}
+	for i := 0; i < h; i++ {
+		for j := 0; j < w; j++ {
+			if handler(0, i, j) {
+				return true
+			}
+		}
+	}
+	return false
+}
+
 func returnValues() (result int) {
 	//var result int
 	defer func() {
