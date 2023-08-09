12de799f298a7731ba1163072bb4b992dc88a23a
Derek <237026504@163.com>
2023-08-10 00:10:52 +0800

    338

--- dp/countBits/338.go
+++ dp/countBits/338.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func countBits(n int) []int {
 	bit := make([]int, n+1)
@@ -112,6 +114,18 @@ func nine(n int) []int {
 }
 
 func ten(n int) []int {
+	bit := make([]int, n+1)
+	highBit := 0
+	for i := 1; i <= n; i++ {
+		if i&(i-1) == 0 {
+			highBit = i
+		}
+		bit[i] = bit[i-highBit] + 1
+	}
+	return bit
+}
+
+func eleven(n int) []int {
 	bit := make([]int, n+1)
 	highBit := 0
 	for i := 1; i <= n; i++ {
