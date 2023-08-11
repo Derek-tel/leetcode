dc5936b81f5a6ce228b19c64050d1ab6dff1a255
Derek <237026504@163.com>
2023-08-12 00:54:20 +0800

    279

--- dp/numSquares/279.go
+++ dp/numSquares/279.go
@@ -113,6 +113,18 @@ func nine(n int) int {
 	return dp[n]
 }
 
+func ten(n int) int {
+	dp := make([]int, n+1)
+	dp[0] = 0
+	for i := 1; i <= n; i++ {
+		dp[i] = i
+		for j := 0; j*j <= i; j++ {
+			dp[i] = min(dp[i], dp[i-j*j]+1)
+		}
+	}
+	return dp[n]
+}
+
 func min(i, j int) int {
 	if i > j {
 		return j
