8184fbc96ddcb2b4f230b3f4d7bbfd0b7ff5ed09
Derek <237026504@163.com>
2023-08-14 23:59:23 +0800

    141

--- listedlink/hasCycle/141.go
+++ listedlink/hasCycle/141.go
@@ -98,3 +98,15 @@ func eight(head *ListNode) bool {
 	}
 	return false
 }
+
+func nine(head *ListNode) bool {
+	fast, slow := head, head
+	for fast != nil && fast.Next != nil {
+		fast = fast.Next.Next
+		slow = slow.Next
+		if fast == slow {
+			return true
+		}
+	}
+	return false
+}
