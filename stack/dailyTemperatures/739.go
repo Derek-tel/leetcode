2ba7a27138c9a7e4174f93cc9a9ce95d3c56a7a8
Derek <237026504@163.com>
2023-08-20 00:15:27 +0800

    739

--- stack/dailyTemperatures/739.go
+++ stack/dailyTemperatures/739.go
@@ -140,3 +140,18 @@ func eight(temperatures []int) []int {
 	}
 	return result
 }
+
+func nine(temperatures []int) []int {
+	result := make([]int, len(temperatures))
+	var stack []int
+	for i := 0; i < len(temperatures); i++ {
+		today := temperatures[i]
+		for len(stack) > 0 && today > temperatures[stack[len(stack)-1]] {
+			prev := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			result[prev] = i - prev
+		}
+		stack = append(stack, i)
+	}
+	return result
+}
