9b9b515e97dcb54db89ce6e1b1c9218457f5f63e
Derek <237026504@163.com>
2023-09-10 23:02:45 +0800

    33

--- binary search/search/33.go
+++ binary search/search/33.go
@@ -242,13 +242,38 @@ func nine(nums []int, target int) int {
 
 func ten(nums []int, target int) int {
 	length := len(nums)
-	left, right := 0, len(nums)
+	left, right := 0, len(nums)-1
 	for left <= right {
 		mid := left + (right-left)>>1
 		if nums[mid] == target {
 			return mid
 		}
 
+		if nums[0] <= nums[mid] {
+			if nums[0] <= target && target < nums[mid] {
+				right = mid - 1
+			} else {
+				left = mid + 1
+			}
+		} else {
+			if nums[mid] < target && target <= nums[length-1] {
+				left = mid + 1
+			} else {
+				right = mid - 1
+			}
+		}
+	}
+	return -1
+}
+
+func eleven(nums []int, target int) int {
+	length := len(nums)
+	left, right := 0, length-1
+	for left <= right {
+		mid := left + (right-left)>>1
+		if nums[mid] == target {
+			return mid
+		}
 		if nums[0] <= nums[mid] {
 			if nums[0] <= target && target < nums[mid] {
 				right = mid - 1
