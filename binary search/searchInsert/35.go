f92f64f541d056304f2f93660252470819be0722
Derek <237026504@163.com>
2023-09-11 23:17:40 +0800

    35

--- binary search/searchInsert/35.go
+++ binary search/searchInsert/35.go
@@ -81,3 +81,20 @@ func ten(nums []int, target int) int {
 	}
 	return len(nums)
 }
+
+func eleven(nums []int, target int) int {
+	left, right := 0, len(nums)-1
+	for left <= right {
+		mid := left + (right-left)>>1
+
+		if nums[mid] >= target {
+			if mid == 0 || nums[mid-1] < target {
+				return mid
+			}
+			right = mid - 1
+		} else {
+			left = mid + 1
+		}
+	}
+	return len(nums)
+}
