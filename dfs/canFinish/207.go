6d14c5873d8fa30baeeb58c6d1de5d51586ca0b7
Derek <237026504@163.com>
2023-09-17 23:20:18 +0800

    207

--- dfs/canFinish/207.go
+++ dfs/canFinish/207.go
@@ -336,3 +336,34 @@ func ten(numCourses int, prerequisites [][]int) bool {
 	}
 	return valid
 }
+
+func eleven(numCourses int, prerequisites [][]int) bool {
+	var valid = true
+	var visit = make([]int, numCourses)
+	var edge = make([][]int, numCourses)
+	var helper func(int)
+	helper = func(course int) {
+		visit[course] = 1
+		for _, site := range edge[course] {
+			if visit[site] == 0 {
+				helper(site)
+				if valid == false {
+					return
+				}
+			} else if visit[site] == 1 {
+				valid = false
+				return
+			}
+		}
+		visit[course] = 2
+	}
+	for _, site := range prerequisites {
+		edge[site[1]] = append(edge[site[1]], site[0])
+	}
+	for i := 0; i < numCourses && valid; i++ {
+		if visit[i] == 0 {
+			helper(i)
+		}
+	}
+	return valid
+}
