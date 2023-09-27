f48b254f0ab00c3e71f0dcb26a13c959c9472ce5
Derek <237026504@163.com>
2023-09-27 23:54:51 +0800

    152

--- dp/maxProduct/152.go
+++ dp/maxProduct/152.go
@@ -138,6 +138,19 @@ func ten(nums []int) int {
 	return result
 }
 
+func eleven(nums []int) int {
+	maxN, minN, result := nums[0], nums[0], nums[0]
+
+	for i := 1; i < len(nums); i++ {
+		mm := maxN
+		mn := minN
+		maxN = max(max(nums[i]*mm, nums[i]*mn), nums[i])
+		minN = min(min(nums[i]*mm, nums[i]*mn), nums[i])
+		result = max(result, maxN)
+	}
+	return result
+}
+
 func max(i, j int) int {
 	if i > j {
 		return i
