b116b17cec9740c7d879051c4969a1d1457c4706
Derek <237026504@163.com>
2023-10-12 23:58:13 +0800

    128

--- hashtable/longestConsecutive/128.go
+++ hashtable/longestConsecutive/128.go
@@ -202,3 +202,24 @@ func nine(nums []int) int {
 	}
 	return longest
 }
+
+func ten(nums []int) int {
+	dic := make(map[int]bool)
+	for _, num := range nums {
+		dic[num] = true
+	}
+	longest := 0
+	for _, num := range nums {
+		if !dic[num-1] {
+			current := 1
+			for dic[num+1] {
+				current++
+				num++
+			}
+			if current > longest {
+				longest = current
+			}
+		}
+	}
+	return longest
+}
