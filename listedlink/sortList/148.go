29df583f58aa589a853e3edc26a59a2ae120559a
Derek <237026504@163.com>
2023-10-27 23:51:24 +0800

    148

--- listedlink/sortList/148.go
+++ listedlink/sortList/148.go
@@ -281,6 +281,32 @@ func nine(head *ListNode) *ListNode {
 	return mergeTwo(nine(head), nine(secondHalf))
 }
 
+func ten(head *ListNode) *ListNode {
+	var getMiddle func(*ListNode) *ListNode
+	getMiddle = func(node *ListNode) *ListNode {
+		fast, slow := node, node
+		for fast.Next != nil && fast.Next.Next != nil {
+			fast = fast.Next.Next
+			slow = slow.Next
+		}
+		return slow
+	}
+
+	length := 0
+	cur := head
+	for cur != nil {
+		length++
+		cur = cur.Next
+	}
+	if length <= 1 {
+		return head
+	}
+	middle := getMiddle(head)
+	secondHalf := middle.Next
+	middle.Next = nil
+	return mergeTwo(ten(head), ten(secondHalf))
+}
+
 func mergeTwo(listA, listB *ListNode) *ListNode {
 	preHead := &ListNode{}
 	prev := preHead
