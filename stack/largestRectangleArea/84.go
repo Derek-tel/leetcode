53d19672f1254ebee6ed50e0e8bf4677f8945d31
Derek <237026504@163.com>
2023-11-06 23:40:45 +0800

    84

--- stack/largestRectangleArea/84.go
+++ stack/largestRectangleArea/84.go
@@ -194,6 +194,27 @@ func eight(heights []int) int {
 	return result
 }
 
+func nine(heights []int) int {
+	var stack []int
+	length := len(heights) + 2
+	getHeight := func(i int) int {
+		if i == 0 || i == length-1 {
+			return -1
+		}
+		return heights[i-1]
+	}
+	result := 0
+	for i := 0; i < length; i++ {
+		for len(stack) > 0 && getHeight(i) < getHeight(stack[len(stack)-1]) {
+			top := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			result = max(result, getHeight(top)*(i-stack[len(stack)-1]-1))
+		}
+		stack = append(stack, i)
+	}
+	return result
+}
+
 func max(i, j int) int {
 	if i > j {
 		return i
