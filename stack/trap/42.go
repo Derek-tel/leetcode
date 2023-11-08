1d4e91387dab9f01d0edf041b19fcc8741b74251
Derek <237026504@163.com>
2023-11-08 23:55:32 +0800

    42

--- stack/trap/42.go
+++ stack/trap/42.go
@@ -157,6 +157,25 @@ func eight(height []int) int {
 	return result
 }
 
+func nine(height []int) int {
+	var stack []int
+	var result = 0
+	for index, high := range height {
+		for len(stack) > 0 && high > height[stack[len(stack)-1]] {
+			top := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			if len(stack) == 0 {
+				break
+			}
+			prev := stack[len(stack)-1]
+			weight := index - prev - 1
+			result = result + (min(high, height[prev])-height[top])*weight
+		}
+		stack = append(stack, index)
+	}
+	return result
+}
+
 func min(i, j int) int {
 	if i < j {
 		return i
