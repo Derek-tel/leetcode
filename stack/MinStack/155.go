7a115ebcf5a2be8bc634f43147eb5edef01670a3
Derek <237026504@163.com>
2023-11-08 23:04:53 +0800

    155

--- stack/MinStack/155.go
+++ stack/MinStack/155.go
@@ -3,25 +3,25 @@ package MinStack
 import "math"
 
 type MinStack struct {
-	stack []int
-	min   []int
+	stack    []int
+	minStack []int
 }
 
 func Constructor() MinStack {
 	return MinStack{
-		stack: []int{},
-		min:   []int{math.MaxInt},
+		stack:    []int{},
+		minStack: []int{math.MaxInt},
 	}
 }
 
 func (this *MinStack) Push(val int) {
 	this.stack = append(this.stack, val)
-	this.min = append(this.min, min(this.min[len(this.min)-1], val))
+	this.minStack = append(this.minStack, this.min(this.minStack[len(this.minStack)-1], val))
 }
 
 func (this *MinStack) Pop() {
 	this.stack = this.stack[:len(this.stack)-1]
-	this.min = this.min[:len(this.min)-1]
+	this.minStack = this.minStack[:len(this.minStack)-1]
 }
 
 func (this *MinStack) Top() int {
@@ -29,7 +29,14 @@ func (this *MinStack) Top() int {
 }
 
 func (this *MinStack) GetMin() int {
-	return this.min[len(this.min)-1]
+	return this.minStack[len(this.minStack)-1]
+}
+
+func (this *MinStack) min(i, j int) int {
+	if i < j {
+		return i
+	}
+	return j
 }
 
 /**
@@ -40,10 +47,3 @@ func (this *MinStack) GetMin() int {
  * param_3 := obj.Top();
  * param_4 := obj.GetMin();
  */
-
-func min(x, j int) int {
-	if x < j {
-		return x
-	}
-	return j
-}
