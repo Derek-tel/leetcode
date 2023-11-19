60d68c9883080f9fc2cf94e6ada3d84a718ba52f
Derek-tel <237026504@163.com>
2023-11-19 22:43:21 +0800

    617

--- tree/mergeTrees/617.go
+++ tree/mergeTrees/617.go
@@ -130,3 +130,16 @@ func nine(root1, root2 *TreeNode) *TreeNode {
 	root1.Right = nine(root1.Right, root2.Right)
 	return root1
 }
+
+func ten(root1, root2 *TreeNode) *TreeNode {
+	if root1 == nil {
+		return root2
+	}
+	if root2 == nil {
+		return root1
+	}
+	root1.Val = root1.Val + root2.Val
+	root1.Left = ten(root1.Left, root2.Left)
+	root1.Right = ten(root1.Right, root2.Right)
+	return root1
+}
