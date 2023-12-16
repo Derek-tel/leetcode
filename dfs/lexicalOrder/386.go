2d27563178f1827458750ee2e9093a6a3980d403
Derek <237026504@163.com>
2023-12-17 01:48:48 +0800

    386

---
+++ dfs/lexicalOrder/386.go
@@ -0,0 +1,18 @@
+package lexicalOrder
+
+func one(n int) []int {
+	ans := make([]int, n)
+	num := 1
+	for i := range ans {
+		ans[i] = num
+		if num*10 <= n {
+			num = num * 10
+		} else {
+			for num%10 == 9 || num+1 > n {
+				num /= 10
+			}
+			num++
+		}
+	}
+	return ans
+}
