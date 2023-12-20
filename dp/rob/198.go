f9f94c731bc4749095a0e9760aff23861592f73a
Derek <237026504@163.com>
2023-12-20 23:54:42 +0800

    337

--- dp/rob/198.go
+++ dp/rob/198.go
@@ -176,13 +176,13 @@ func eleven198(nums []int) int {
 	if len(nums) == 1 {
 		return nums[0]
 	}
-	dp := make([]int, len(nums))
-	dp[0] = nums[0]
-	dp[1] = max(nums[0], nums[1])
-	for i := 2; i < len(nums); i++ {
-		dp[i] = max(dp[i-1], dp[i-2]+nums[i])
+	dp := make([]int, len(nums)+1)
+	dp[0] = 0
+	dp[1] = nums[1]
+	for i := 2; i <= len(nums); i++ {
+		dp[i] = max(dp[i-1], dp[i-2]+nums[i-1])
 	}
-	return dp[len(nums)-1]
+	return dp[len(nums)]
 }
 func max(i, j int) int {
 	if i > j {
