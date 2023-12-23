fb9c04b6ab98415ad9796186a86b9234279847b0
Derek <237026504@163.com>
2023-12-23 23:49:25 +0800

    19

--- listedlink/removeNthFromEnd/19.go
+++ listedlink/removeNthFromEnd/19.go
@@ -216,3 +216,21 @@ func eleven(head *ListNode, n int) *ListNode {
 	slow.Next = slow.Next.Next
 	return preHead
 }
+
+func twelve(head *ListNode, n int) *ListNode {
+	if head == nil {
+		return head
+	}
+	preHead := &ListNode{Next: head}
+	slow := preHead
+	fast := head
+	for i := 1; i <= n && fast != nil; i++ {
+		fast = fast.Next
+	}
+	for fast != nil {
+		fast = fast.Next
+		slow = slow.Next
+	}
+	slow.Next = slow.Next.Next
+	return preHead.Next
+}
