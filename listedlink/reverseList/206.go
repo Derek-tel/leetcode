8d4b4f57d62fbf9fc75b38084c6ed70d0ac9736d
Derek <237026504@163.com>
2023-12-24 00:14:23 +0800

    206

--- listedlink/reverseList/206.go
+++ listedlink/reverseList/206.go
@@ -159,3 +159,19 @@ func nine(head *ListNode) *ListNode {
 	}
 	return dumpy.Next
 }
+
+func ten(head *ListNode) *ListNode {
+	if head == nil {
+		return head
+	}
+	dumpy := &ListNode{Next: head}
+	prev := dumpy
+	cur := head
+	for cur.Next != nil {
+		next := cur.Next
+		cur.Next = next.Next
+		next.Next = prev.Next
+		prev.Next = next
+	}
+	return dumpy.Next
+}
