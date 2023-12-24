6419c8ce8af7db5b841a3c04b97174680253c01d
Derek <237026504@163.com>
2023-12-24 23:56:55 +0800

    7

--- math/reverse/7.go
+++ math/reverse/7.go
@@ -1,6 +1,8 @@
 package reverse
 
-import "math"
+import (
+	"math"
+)
 
 func reverse(x int) int {
 	var result int
@@ -84,6 +86,19 @@ func six(x int) int {
 }
 
 func seven(x int) int {
+	var result int
+	for x != 0 {
+		if result < math.MinInt32/10 || result > math.MaxInt32/10 {
+			return 0
+		}
+		digit := x % 10
+		x = x / 10
+		result = result*10 + digit
+	}
+	return result
+}
+
+func eight(x int) int {
 	var result int
 	for x != 0 {
 		if result < math.MinInt32/10 || result > math.MaxInt32/10 {
