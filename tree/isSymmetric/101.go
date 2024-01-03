86d87c331bd18c96b55d702bb1991c83377de5e3
Derek <237026504@163.com>
2024-01-03 23:36:31 +0800

    101

--- tree/isSymmetric/101.go
+++ tree/isSymmetric/101.go
@@ -176,3 +176,17 @@ func eleven(root *TreeNode) bool {
 	}
 	return handler(root.Left, root.Right)
 }
+
+func twelve(root *TreeNode) bool {
+	var handler func(*TreeNode, *TreeNode) bool
+	handler = func(left *TreeNode, right *TreeNode) bool {
+		if left == nil && right == nil {
+			return true
+		}
+		if left == nil || right == nil {
+			return false
+		}
+		return left.Val == right.Val && handler(left.Left, right.Right) && handler(left.Right, right.Left)
+	}
+	return handler(root.Left, root.Right)
+}
