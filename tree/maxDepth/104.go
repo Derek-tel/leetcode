9bad5dff18537e5fd9015825c220b8485a7f8618
Derek <237026504@163.com>
2024-01-04 23:14:06 +0800

    104

--- tree/maxDepth/104.go
+++ tree/maxDepth/104.go
@@ -86,6 +86,15 @@ func eleven(root *TreeNode) int {
 	return max(left, right) + 1
 }
 
+func twelve(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	left := twelve(root.Left)
+	right := twelve(root.Right)
+	return max(left, right) + 1
+}
+
 func max(i int, j int) int {
 	if i > j {
 		return i
