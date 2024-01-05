a108e910da8ab6c9077c206518207c9406feab5b
Derek <237026504@163.com>
2024-01-05 22:50:32 +0800

    144

--- tree/preorderTraversal/144.go
+++ tree/preorderTraversal/144.go
@@ -165,3 +165,19 @@ func ten(root *TreeNode) []int {
 	}
 	return result
 }
+
+func eleven(root *TreeNode) []int {
+	var result []int
+	var stack []*TreeNode
+	for len(stack) > 0 || root != nil {
+		for root != nil {
+			stack = append(stack, root)
+			result = append(result, root.Val)
+			root = root.Left
+		}
+		top := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		root = top.Right
+	}
+	return result
+}
