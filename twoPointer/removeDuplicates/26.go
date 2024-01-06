dbd7e3a964a0d27389d17a09366432df612271f1
Derek <237026504@163.com>
2024-01-06 23:13:42 +0800

    26

--- twoPointer/removeDuplicates/26.go
+++ twoPointer/removeDuplicates/26.go
@@ -85,3 +85,14 @@ func five(nums []int) int {
 	}
 	return slow
 }
+
+func six(nums []int) int {
+	slow := 1
+	for fast := 1; fast < len(nums); fast++ {
+		if nums[fast] != nums[fast-1] {
+			nums[slow] = nums[fast]
+			slow++
+		}
+	}
+	return slow
+}
