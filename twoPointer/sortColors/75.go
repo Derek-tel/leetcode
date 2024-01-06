131c59c91e03449b96d1c5b6e3ddc48f6e14890c
Derek <237026504@163.com>
2024-01-06 23:19:53 +0800

    75

--- twoPointer/sortColors/75.go
+++ twoPointer/sortColors/75.go
@@ -146,6 +146,19 @@ func nine(nums []int) {
 	}
 }
 
+func ten(nums []int) {
+	p0, p2 := 0, len(nums)-1
+	for i := 0; i <= p2; i++ {
+		for ; i <= p2 && nums[i] == 2; p2-- {
+			nums[i], nums[p2] = nums[p2], nums[i]
+		}
+		if nums[i] == 0 {
+			nums[i], nums[p0] = nums[p0], nums[i]
+			p0++
+		}
+	}
+}
+
 func main() {
 	test := []int{2, 0, 2, 1, 1, 0}
 	sortColor(test)
