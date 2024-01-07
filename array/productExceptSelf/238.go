51b45b3c7a176b71cc2a230001379bd2f38facf8
Derek <237026504@163.com>
2024-01-07 23:43:04 +0800

    238

--- array/productExceptSelf/238.go
+++ array/productExceptSelf/238.go
@@ -152,6 +152,21 @@ func ten(nums []int) []int {
 	return result
 }
 
+func eleven(nums []int) []int {
+	length := len(nums)
+	result := make([]int, length)
+	result[0] = 1
+	for i := 1; i < length; i++ {
+		result[i] = result[i-1] * nums[i-1]
+	}
+	r := 1
+	for i := length - 1; i >= 0; i-- {
+		result[i] = result[i] * r
+		r = r * nums[i]
+	}
+	return result
+}
+
 func main() {
 	fmt.Println(productExceptSelf([]int{1, 2, 3, 4}))
 }
