48778c7804ac1cd2ce1d6b9dd26c2e9b38f6a1a7
Derek <237026504@163.com>
2024-01-07 23:31:16 +0800

    59

--- array/generateMatrix/59.go
+++ array/generateMatrix/59.go
@@ -1,11 +1,5 @@
 package generateMatrix
 
-type pair struct {
-	x, y int
-}
-
-var pairList = []pair{{0, 1}, {1, 0}, {0, -1}, {-1, 0}}
-
 func one(n int) [][]int {
 	matrix := make([][]int, n)
 	for i := range matrix {
@@ -24,3 +18,28 @@ func one(n int) [][]int {
 	}
 	return matrix
 }
+
+func two(n int) [][]int {
+	matrix := make([][]int, n)
+	for i := 0; i < len(matrix); i++ {
+		matrix[i] = make([]int, n)
+	}
+	row, col, dirIndex := 0, 0, 0
+	for i := 1; i <= n*n; i++ {
+		matrix[row][col] = i
+		dir := pairList[dirIndex]
+		if r, c := row+dir.x, col+dir.y; r < 0 || r >= n || c < 0 || c >= n || matrix[r][c] > 0 {
+			dirIndex = (dirIndex + 1) % 4
+			dir = pairList[dirIndex]
+		}
+		row = row + dir.x
+		col = col + dir.y
+	}
+	return matrix
+}
+
+type pair struct {
+	x, y int
+}
+
+var pairList = []pair{{0, 1}, {1, 0}, {0, -1}, {-1, 0}}
