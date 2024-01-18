db87285c3c72a20ddfe2b32a2468ed963a4990a7
Derek <237026504@163.com>
2024-01-19 00:01:19 +0800

    121

--- dp/maxProfit/121.go
+++ dp/maxProfit/121.go
@@ -231,3 +231,21 @@ func thirteen(price []int) int {
 	}
 	return result
 }
+
+func fourteen(price []int) int {
+	if len(price) == 0 {
+		return 0
+	}
+	var result int
+	min := price[0]
+
+	for i := 1; i < len(price); i++ {
+		if price[i]-min > result {
+			result = price[i] - min
+		}
+		if price[i] < min {
+			min = price[i]
+		}
+	}
+	return result
+}
