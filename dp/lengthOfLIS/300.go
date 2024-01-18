295e78ed50e399a7be7bd2531fcb604e0b92e7dd
Derek <237026504@163.com>
2024-01-18 23:26:09 +0800

    300

--- dp/lengthOfLIS/300.go
+++ dp/lengthOfLIS/300.go
@@ -227,6 +227,22 @@ func fourteen(nums []int) int {
 	return result
 }
 
+func fifteen(nums []int) int {
+	dp := make([]int, len(nums))
+	result := 1
+	dp[0] = 1
+	for i := 1; i < len(nums); i++ {
+		dp[i] = 1
+		for j := 0; j < i; j++ {
+			if nums[j] < nums[i] {
+				dp[i] = max(dp[i], dp[j]+1)
+			}
+		}
+		result = max(result, dp[i])
+	}
+	return result
+}
+
 func max(i, j int) int {
 	if i > j {
 		return i
