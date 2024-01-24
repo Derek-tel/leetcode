9e857828a863ff3a33da7e2dc8dfc47df2424758
Derek <237026504@163.com>
2024-01-24 23:50:10 +0800

    86

--- listedlink/partition/86.go
+++ listedlink/partition/86.go
@@ -236,3 +236,23 @@ func eleven(head *ListNode, x int) *ListNode {
 	largeHead.Next = nil
 	return small.Next
 }
+
+func twelve(head *ListNode, x int) *ListNode {
+	small, large := &ListNode{}, &ListNode{}
+	smallHead, largeHead := small, large
+
+	for head != nil {
+		if head.Val < x {
+			smallHead.Next = head
+			smallHead = smallHead.Next
+		} else {
+			largeHead.Next = head
+			largeHead = largeHead.Next
+		}
+		head = head.Next
+	}
+	smallHead.Next = large.Next
+	largeHead.Next = nil
+
+	return small.Next
+}
