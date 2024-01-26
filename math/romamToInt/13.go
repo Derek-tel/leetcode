5c41301ed6c4dc2bbb0b8528f38da856dc3ea889
Derek <237026504@163.com>
2024-01-26 23:20:03 +0800

    13

---
+++ math/romamToInt/13.go
@@ -0,0 +1,25 @@
+package romamToInt
+
+var symbolValue = map[byte]int{
+	'I': 1,
+	'V': 5,
+	'X': 10,
+	'L': 50,
+	'C': 100,
+	'D': 500,
+	'M': 1000,
+}
+
+func one(s string) int {
+	result := 0
+	n := len(s)
+	for i := range s {
+		value := symbolValue[s[i]]
+		if i < n-1 && value < symbolValue[s[i+1]] {
+			result -= value
+		} else {
+			result += value
+		}
+	}
+	return result
+}
