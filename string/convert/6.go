0813ff00bc8a1e9add84762f67f5b387dfb4bb80
Derek <237026504@163.com>
2024-01-31 23:57:22 +0800

    6

--- string/convert/6.go
+++ string/convert/6.go
@@ -1,6 +1,8 @@
 package convert
 
-import "bytes"
+import (
+	"bytes"
+)
 
 func convert(s string, numRows int) string {
 	if numRows < 2 || numRows >= len(s) {
@@ -26,6 +28,22 @@ func two(s string, numRow int) string {
 	i, flag := 0, -1
 	for _, ch := range s {
 		snake[i] = append(snake[i], byte(ch))
+		if i == 0 || i == numRow-1 {
+			flag = -flag
+		}
+		i += flag
+	}
+	return string(bytes.Join(snake, nil))
+}
+
+func three(s string, numRow int) string {
+	if numRow < 2 || numRow >= len(s) {
+		return s
+	}
+	snake := make([][]byte, numRow)
+	i, flag := 0, -1
+	for _, v := range s {
+		snake[i] = append(snake[i], byte(v))
 		if i == 0 || i == numRow-1 {
 			flag = -flag
 		}
