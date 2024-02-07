839ea61c455dad851450ff0c5bbc58f048e10221
Derek <237026504@163.com>
2024-02-07 14:24:29 +0800

    27

--- twoPointer/removeElement/27.go
+++ twoPointer/removeElement/27.go
@@ -78,3 +78,14 @@ func six(nums []int, val int) int {
 	}
 	return flag
 }
+
+func seven(nums []int, val int) int {
+	flag := 0
+	for i := 0; i < len(nums); i++ {
+		if nums[i] != val {
+			nums[i], nums[flag] = nums[flag], nums[i]
+			flag++
+		}
+	}
+	return flag
+}
