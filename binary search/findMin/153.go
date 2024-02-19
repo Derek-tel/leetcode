93cefcd8c820e14b92d0cb9e4b87be0e89521561
Derek <237026504@163.com>
2024-02-20 00:03:21 +0800

    153

--- binary search/findMin/153.go
+++ binary search/findMin/153.go
@@ -29,3 +29,18 @@ func twe(nums []int) int {
 	}
 	return nums[low]
 }
+
+func three(nums []int) int {
+	low, high := 0, len(nums)-1
+	for low < high {
+		mid := low + (high-low)>>1
+		if nums[mid] < nums[high] {
+			high = mid
+		} else if nums[mid] > nums[high] {
+			low = mid + 1
+		} else {
+			high = mid - 1
+		}
+	}
+	return nums[low]
+}
