e541aac316dcf17c481fd0495d6bc5cca547d000
Derek <237026504@163.com>
2024-02-20 23:20:06 +0800

    35

--- binary search/searchInsert/35.go
+++ binary search/searchInsert/35.go
@@ -130,3 +130,19 @@ func thirteen(nums []int, target int) int {
 	}
 	return len(nums)
 }
+
+func fourteen(nums []int, target int) int {
+	left, right := 0, len(nums)-1
+	for left <= right {
+		mid := left + (right-left)>>1
+		if nums[mid] >= target {
+			if mid == 0 || nums[mid-1] < target {
+				return mid
+			}
+			right = mid - 1
+		} else {
+			left = mid + 1
+		}
+	}
+	return len(nums)
+}
