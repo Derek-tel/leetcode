e3cc80235e1c46dbf182026bc208565d41d54258
Derek <237026504@163.com>
2024-02-21 23:59:41 +0800

    70

--- dp/climbStairs/70.go
+++ dp/climbStairs/70.go
@@ -117,3 +117,13 @@ func twelve(n int) int {
 	}
 	return dp[n]
 }
+
+func thirteen(n int) int {
+	dp := make([]int, n+1)
+	dp[0] = 1
+	dp[1] = 1
+	for i := 2; i <= n; i++ {
+		dp[i] = dp[i-1] + dp[i-2]
+	}
+	return dp[n]
+}
