204743c63fcc80d6f902d9b5a0c6b4b9bab3d6fa
Derek <237026504@163.com>
2024-02-24 00:12:07 +0800

    221

--- dp/maximalSquare/221.go
+++ dp/maximalSquare/221.go
@@ -300,6 +300,29 @@ func thirteen(matrix [][]byte) int {
 	return maxSide * maxSide
 }
 
+func fourteen(matrix [][]byte) int {
+	dp := make([][]int, len(matrix))
+	maxSide := 0
+	for i := 0; i < len(dp); i++ {
+		dp[i] = make([]int, len(matrix[i]))
+		for j := 0; j < len(matrix[i]); j++ {
+			if matrix[i][j]-'0' == 1 {
+				dp[i][j] = 1
+				maxSide = 1
+			}
+		}
+	}
+	for i := 1; i < len(dp); i++ {
+		for j := 1; j < len(dp[i]); j++ {
+			if dp[i][j] == 1 {
+				dp[i][j] = min(min(dp[i-1][j], dp[i][j-1]), dp[i-1][j-1]) + 1
+			}
+			maxSide = max(maxSide, dp[i][j])
+		}
+	}
+	return maxSide * maxSide
+}
+
 func min(i, j int) int {
 	if i > j {
 		return j
