aeaa1a59db6014d2f786eda8f7b40f429502719e
Derek <237026504@163.com>
2024-02-24 23:47:53 +0800

    198

--- dp/rob/198.go
+++ dp/rob/198.go
@@ -200,6 +200,23 @@ func twelve198(nums []int) int {
 	}
 	return dp[len(nums)]
 }
+
+func thirteen198(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+	if len(nums) == 1 {
+		return nums[0]
+	}
+	dp := make([]int, len(nums)+1)
+	dp[0] = 0
+	dp[1] = nums[0]
+	for i := 2; i <= len(nums); i++ {
+		dp[i] = max(dp[i-1], dp[i-2]+nums[i-1])
+	}
+	return dp[len(nums)]
+}
+
 func max(i, j int) int {
 	if i > j {
 		return i
