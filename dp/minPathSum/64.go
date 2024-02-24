e9483bdc9371122bd5fecfa01ca297033bac04ac
Derek <237026504@163.com>
2024-02-24 23:35:08 +0800

    64

--- dp/minPathSum/64.go
+++ dp/minPathSum/64.go
@@ -383,6 +383,38 @@ func twelve(grid [][]int) int {
 	}
 	return dp[row-1][col-1]
 }
+
+func thirteen(grid [][]int) int {
+	row, col := len(grid), len(grid[0])
+	if row == 0 || col == 0 {
+		return 0
+	}
+	dp := make([][]int, row)
+	for i := 0; i < len(dp); i++ {
+		dp[i] = make([]int, col)
+	}
+	for i := 0; i < row; i++ {
+		if i == 0 {
+			dp[i][0] = grid[0][0]
+		} else {
+			dp[i][0] = dp[i-1][0] + grid[i][0]
+		}
+	}
+	for i := 0; i < col; i++ {
+		if i == 0 {
+			dp[0][i] = grid[0][0]
+		} else {
+			dp[0][i] = dp[0][i-1] + grid[0][i]
+		}
+	}
+
+	for i := 1; i < row; i++ {
+		for j := 1; j < col; j++ {
+			dp[i][j] = min(dp[i-1][j], dp[i][j-1]) + grid[i][j]
+		}
+	}
+	return dp[row-1][col-1]
+}
 func min(i, j int) int {
 	if i < j {
 		return i
