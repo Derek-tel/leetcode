bd6fcff21f474a9cb9c0a486ebf8570224bbf2ae
Derek <237026504@163.com>
2024-02-27 23:46:08 +0800

    83

--- listedlink/deleteDuplicates/83.go
+++ listedlink/deleteDuplicates/83.go
@@ -162,6 +162,21 @@ func eight(head *ListNode) *ListNode {
 	return head
 }
 
+func nine(head *ListNode) *ListNode {
+	if head == nil {
+		return head
+	}
+	cur := head
+	for cur.Next != nil {
+		if cur.Val == cur.Next.Val {
+			cur.Next = cur.Next.Next
+		} else {
+			cur = cur.Next
+		}
+	}
+	return head
+}
+
 func main() {
 	node4 := &ListNode{1, nil}
 	node3 := &ListNode{3, node4}
