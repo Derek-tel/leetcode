75246d9484add6506b5bfab04d95e97533198c7c
Derek <237026504@163.com>
2024-02-29 00:25:32 +0800

    21

--- listedlink/mergeTwoLists/21.go
+++ listedlink/mergeTwoLists/21.go
@@ -268,3 +268,24 @@ func thirteen(list1 *ListNode, list2 *ListNode) *ListNode {
 	}
 	return preHead.Next
 }
+
+func fourteen(list1, list2 *ListNode) *ListNode {
+	preHead := &ListNode{}
+	prev := preHead
+	for list1 != nil && list2 != nil {
+		if list1.Val < list2.Val {
+			prev.Next = list1
+			list1 = list1.Next
+		} else {
+			prev.Next = list2
+			list2 = list2.Next
+		}
+		prev = prev.Next
+	}
+	if list1 != nil {
+		prev.Next = list1
+	} else {
+		prev.Next = list2
+	}
+	return preHead.Next
+}
