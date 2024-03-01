8698240613575da9ab6b910003f87d7e7f6b203a
Derek <237026504@163.com>
2024-03-01 23:53:22 +0800

    9

--- math/isPalindrome/9.go
+++ math/isPalindrome/9.go
@@ -104,6 +104,18 @@ func seven(x int) bool {
 	return x == flag || x == flag/10
 }
 
+func eight(x int) bool {
+	if x < 0 || (x%10 == 0 && x != 0) {
+		return false
+	}
+	flag := 0
+	for x > flag {
+		flag = flag*10 + x%10
+		x = x / 10
+	}
+	return x == flag || x == flag/10
+}
+
 func main() {
 	x := 10101
 	fmt.Println(one(x))
