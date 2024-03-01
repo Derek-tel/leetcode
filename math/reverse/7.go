c08c507c6d82bda6182f2e82c35d28c1ffd80785
Derek <237026504@163.com>
2024-03-01 23:59:14 +0800

    7

--- math/reverse/7.go
+++ math/reverse/7.go
@@ -123,3 +123,16 @@ func nine(x int) int {
 	}
 	return result
 }
+
+func ten(x int) int {
+	var result int
+	for x != 0 {
+		if result*10 < math.MinInt32 || result*10 > math.MaxInt32 {
+			return 0
+		}
+		digit := x % 10
+		x = x / 10
+		result = result*10 + digit
+	}
+	return result
+}
