33059a6b4568d4f8aec4d25497817fe76889146a
Derek <237026504@163.com>
2024-03-02 23:13:16 +0800

    253

--- sort/meeting/253.go
+++ sort/meeting/253.go
@@ -435,6 +435,26 @@ func twelve(interval [][]int) int {
 	return len(end)
 }
 
+func thirteen(interval [][]int) int {
+	sort.Slice(interval, func(i, j int) bool {
+		return interval[i][0] < interval[i][1]
+	})
+	var end []int
+	for i := 0; i < len(interval); i++ {
+		if len(end) == 0 {
+			end = append(end, interval[i][1])
+		} else {
+			if interval[i][0] < end[0] {
+				end = append(end, interval[i][1])
+			} else {
+				end[0] = interval[i][1]
+			}
+		}
+		heap(end)
+	}
+	return len(end)
+}
+
 func main() {
 
 	test := [][]int{
