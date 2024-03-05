d64501359a02ae80d6300bd5156d465275d1b316
Derek <237026504@163.com>
2024-03-05 23:57:10 +0800

    22

--- stack/longestValidParentheses/32.go
+++ stack/longestValidParentheses/32.go
@@ -245,6 +245,24 @@ func twelve(s string) int {
 	return result
 }
 
+func thirteen(s string) int {
+	stack := []int{-1}
+	result := 0
+	for i := 0; i < len(s); i++ {
+		if s[i] == '(' {
+			stack = append(stack, i)
+		} else {
+			stack = stack[:len(stack)-1]
+			if len(stack) == 0 {
+				stack = append(stack, i)
+			} else {
+				result = max(result, i-stack[len(stack)-1])
+			}
+		}
+	}
+	return result
+}
+
 func max(i, j int) int {
 	if i > j {
 		return i
