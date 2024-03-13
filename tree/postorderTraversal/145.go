5a912ea4853b0894c82ea7d867214f31cc9ec540
Derek <237026504@163.com>
2024-03-13 23:59:49 +0800

    145

--- tree/postorderTraversal/145.go
+++ tree/postorderTraversal/145.go
@@ -203,3 +203,25 @@ func eight(root *TreeNode) []int {
 	}
 	return result
 }
+
+func nine(root *TreeNode) []int {
+	var result []int
+	var stack []*TreeNode
+	var prev *TreeNode
+	for len(stack) > 0 || root != nil {
+		for root != nil {
+			stack = append(stack, root)
+			root = root.Left
+		}
+		top := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		if top.Right == nil || prev == top.Right {
+			result = append(result, top.Val)
+			prev = top
+		} else {
+			stack = append(stack, top)
+			root = top.Right
+		}
+	}
+	return result
+}
