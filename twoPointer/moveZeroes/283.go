a9337a5e9d256af58c474d506d5793d86b851f6a
Derek <237026504@163.com>
2024-03-15 23:47:55 +0800

    283

--- twoPointer/moveZeroes/283.go
+++ twoPointer/moveZeroes/283.go
@@ -141,3 +141,13 @@ func twelve(nums []int) {
 		}
 	}
 }
+
+func thirteen(nums []int) {
+	flag := 0
+	for i := 0; i < len(nums); i++ {
+		if nums[i] != 0 {
+			nums[i], nums[flag] = nums[flag], nums[i]
+			flag++
+		}
+	}
+}
