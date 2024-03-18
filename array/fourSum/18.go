0290fba13f89941b18cbcac67dadbc97b49355ff
Derek <237026504@163.com>
2024-03-18 23:53:12 +0800

    18

--- array/fourSum/18.go
+++ array/fourSum/18.go
@@ -530,6 +530,39 @@ func sixteen(nums []int, target int) [][]int {
 	return result
 }
 
+func seventeen(nums []int, target int) [][]int {
+	sort.Ints(nums)
+	length := len(nums)
+	var result [][]int
+	for i := 0; i < length-3 && nums[i]+nums[i+1]+nums[i+2]+nums[i+3] <= target; i++ {
+		if i > 0 && nums[i] == nums[i-1] || nums[i]+nums[length-1]+nums[length-2]+nums[length-3] < target {
+			continue
+		}
+		for j := i + 1; j < length-2 && nums[i]+nums[j]+nums[j+1]+nums[j+2] <= target; j++ {
+			if j > i+1 && nums[j] == nums[j-1] || nums[i]+nums[j]+nums[length-1]+nums[length-2] < target {
+				continue
+			}
+			for left, right := j+1, length-1; left < right; {
+				sum := nums[i] + nums[j] + nums[left] + nums[right]
+				if sum == target {
+					result = append(result, []int{nums[i], nums[j], nums[left], nums[right]})
+					for left++; nums[left] == nums[left-1] && left < right; left++ {
+						continue
+					}
+					for right--; nums[right] == nums[right+1] && left < right; right-- {
+						continue
+					}
+				} else if sum < target {
+					left++
+				} else {
+					right--
+				}
+			}
+		}
+	}
+	return result
+}
+
 func main() {
 	test := []int{-1, 0, 0, 2, 0}
 	result := fourSum(test, 2)
