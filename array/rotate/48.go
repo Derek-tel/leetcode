13dfc595d8dfdee8205d6ef05b8c5d25b16d9438
Derek <237026504@163.com>
2024-03-19 00:10:04 +0800

    48

--- array/rotate/48.go
+++ array/rotate/48.go
@@ -170,3 +170,15 @@ func thirteen(matrix [][]int) {
 		}
 	}
 }
+
+func fourteen(matrix [][]int) {
+	length := len(matrix)
+	for i := 0; i < len(matrix)/2; i++ {
+		matrix[i], matrix[length-i-1] = matrix[length-i-1], matrix[i]
+	}
+	for i := 0; i < length; i++ {
+		for j := 0; j <= i; j++ {
+			matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+		}
+	}
+}
