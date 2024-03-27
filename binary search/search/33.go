e5bf86d49e70cf46108219f0382b8a64d56dfdb9
Derek <237026504@163.com>
2024-03-28 00:07:01 +0800

    33

--- binary search/search/33.go
+++ binary search/search/33.go
@@ -372,6 +372,34 @@ func fourteen(nums []int, target int) int {
 	return -1
 }
 
+func fifteen(nums []int, target int) int {
+	length := len(nums)
+	left, right := 0, length-1
+	for left <= right {
+		mid := left + (right-left)>>1
+		if nums[mid] == target {
+			return mid
+		}
+		if nums[mid] == nums[left] && nums[mid] == nums[right] {
+			left++
+			right--
+		} else if nums[left] <= nums[mid] {
+			if nums[left] <= target && target < nums[mid] {
+				right = mid - 1
+			} else {
+				left = mid + 1
+			}
+		} else {
+			if nums[mid] < target && target <= nums[right] {
+				left = mid + 1
+			} else {
+				right = mid - 1
+			}
+		}
+	}
+	return -1
+}
+
 func main() {
 	test := []int{3, 1}
 	fmt.Println(search(test, 1))
