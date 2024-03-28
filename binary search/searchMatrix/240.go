96b8b462f7d02480d397da17339058a5bc3d231a
Derek <237026504@163.com>
2024-03-28 23:49:00 +0800

    240

--- binary search/searchMatrix/240.go
+++ binary search/searchMatrix/240.go
@@ -241,3 +241,18 @@ func fifteen(matrix [][]int, target int) bool {
 	}
 	return false
 }
+
+func sixteen(matrix [][]int, target int) bool {
+	row, col := len(matrix), len(matrix[0])
+	x, y := 0, col-1
+	for x < row && y >= 0 {
+		if matrix[x][y] == target {
+			return true
+		} else if matrix[x][y] < target {
+			x++
+		} else {
+			y--
+		}
+	}
+	return false
+}
