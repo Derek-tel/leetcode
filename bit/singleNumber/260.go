465d7936fbee7eec1a9fa7dfb4803a82396afaa6
Derek <237026504@163.com>
2024-03-29 23:29:01 +0800

    260

--- bit/singleNumber/260.go
+++ bit/singleNumber/260.go
@@ -40,6 +40,24 @@ func two(nums []int) []int {
 	return []int{left, right}
 }
 
+func fourteen(nums []int) []int {
+	tag := 0
+	for i := 0; i < len(nums); i++ {
+		tag ^= nums[i]
+	}
+	flag := tag & (-tag)
+
+	left, right := 0, 0
+	for _, num := range nums {
+		if num&flag > 0 {
+			left ^= num
+		} else {
+			right ^= num
+		}
+	}
+	return []int{left, right}
+}
+
 func main() {
 	demo := 6
 	fmt.Println(demo & (-demo))
