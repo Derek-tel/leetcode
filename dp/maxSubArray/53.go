1a578ec90ac0ff04db78ac5932aee635bf3d053c
Derek <237026504@163.com>
2024-04-08 23:50:01 +0800

    53

--- dp/maxSubArray/53.go
+++ dp/maxSubArray/53.go
@@ -259,6 +259,24 @@ func fifteen(nums []int) int {
 	return result
 }
 
+func sixteen(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+	dp := make([]int, len(nums))
+	dp[0] = nums[0]
+	result := dp[0]
+	for i := 1; i < len(nums); i++ {
+		if dp[i-1] < 0 {
+			dp[i] = nums[i]
+		} else {
+			dp[i] = dp[i-1] + nums[i]
+		}
+		result = max(result, dp[i])
+	}
+	return result
+}
+
 func max(i, j int) int {
 	if i > j {
 		return i
