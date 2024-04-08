b78782a3017dd48940393ac48744e34874d38ce8
Derek <237026504@163.com>
2024-04-08 23:36:26 +0800

    121

--- dp/maxProfit/121.go
+++ dp/maxProfit/121.go
@@ -267,3 +267,21 @@ func fifteen(price []int) int {
 	}
 	return result
 }
+
+func sixteen(price []int) int {
+	if len(price) == 0 {
+		return 0
+	}
+	var result int
+	min := price[0]
+
+	for i := 1; i < len(price); i++ {
+		if price[i]-min > result {
+			result = price[i] - min
+		}
+		if price[i] < min {
+			min = price[i]
+		}
+	}
+	return result
+}
