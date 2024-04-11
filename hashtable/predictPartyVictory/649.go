c08b9caf9ad4b98881e24f38cd5eb594f51f6213
Derek <237026504@163.com>
2024-04-11 23:29:20 +0800

    update

--- hashtable/predictPartyVictory/649.go
+++ hashtable/predictPartyVictory/649.go
@@ -24,3 +24,27 @@ func one(senate string) string {
 	}
 	return "Dire"
 }
+
+func two(senate string) string {
+	var radiant, dire []int
+	for i, v := range senate {
+		if v == 'R' {
+			radiant = append(radiant, i)
+		} else {
+			dire = append(dire, i)
+		}
+	}
+	for len(radiant) > 0 && len(dire) > 0 {
+		if radiant[0] < dire[0] {
+			radiant = append(radiant, radiant[0]+len(senate))
+		} else {
+			dire = append(dire, dire[0]+len(senate))
+		}
+		radiant = radiant[1:]
+		dire = dire[1:]
+	}
+	if len(radiant) > 0 {
+		return "Radiant"
+	}
+	return "Dire"
+}
