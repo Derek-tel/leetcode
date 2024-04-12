c638716d55fa3ecb0eeac7f1f4816b88ef902876
Derek <237026504@163.com>
2024-04-12 23:35:12 +0800

    142

--- listedlink/detectCycle/142.go
+++ listedlink/detectCycle/142.go
@@ -224,3 +224,19 @@ func thirteen(head *ListNode) *ListNode {
 	}
 	return nil
 }
+
+func fourteen(head *ListNode) *ListNode {
+	fast, slow, p := head, head, head
+	for fast != nil && fast.Next != nil {
+		fast = fast.Next.Next
+		slow = slow.Next
+		if fast == slow {
+			for slow != p {
+				slow = slow.Next
+				p = p.Next
+			}
+			return p
+		}
+	}
+	return nil
+}
