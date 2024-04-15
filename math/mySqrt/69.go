667e37dddfa933a14fa21793863797c7dc3e12a8
Derek <237026504@163.com>
2024-04-15 23:38:09 +0800

    69

--- math/mySqrt/69.go
+++ math/mySqrt/69.go
@@ -134,3 +134,18 @@ func eight(x int) int {
 	}
 	return resp
 }
+
+func nine(x int) int {
+	left, right := 0, x
+	resp := 1
+	for left <= right {
+		mid := left + (right-left)>>1
+		if mid*mid <= x {
+			resp = mid
+			left = mid + 1
+		} else {
+			right = mid - 1
+		}
+	}
+	return resp
+}
