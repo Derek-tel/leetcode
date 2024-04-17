e2fc3a9b7cebe00f8697fd62f03aacf1b4b14518
Derek <237026504@163.com>
2024-04-17 23:53:06 +0800

    912

--- sort/sortArray/912.go
+++ sort/sortArray/912.go
@@ -1,6 +1,8 @@
 package sortArray
 
-import "math/rand"
+import (
+	"math/rand"
+)
 
 func one(nums []int) []int {
 	var handler func([]int, int, int) []int
@@ -76,6 +78,45 @@ func two(nums []int) []int {
 		}
 		return ints
 	}
+	handler(nums, 0, len(nums)-1)
+	return nums
+}
+
+func three(nums []int) []int {
+	var handler func([]int, int, int)
+	handler = func(ints []int, start int, end int) {
+		count := end - start
+		pivot := start
+		if count > 0 {
+			pivot = start + rand.Intn(count)
+		}
+		temp := ints[pivot]
+		i, j := start, end
+		for i <= j {
+			for pivot <= j && ints[j] >= temp {
+				j--
+			}
+			if pivot <= j {
+				ints[j], ints[pivot] = ints[pivot], ints[j]
+				pivot = j
+			}
+			for i <= pivot && ints[i] < temp {
+				i++
+			}
+			if i <= pivot {
+				ints[i], ints[pivot] = ints[pivot], ints[i]
+				pivot = i
+			}
+		}
+		ints[pivot] = temp
+		if pivot-1 > start {
+			handler(ints, start, pivot-1)
+		}
+		if pivot+1 < end {
+			handler(ints, pivot+1, end)
+		}
+	}
+
 	handler(nums, 0, len(nums)-1)
 	return nums
 }
