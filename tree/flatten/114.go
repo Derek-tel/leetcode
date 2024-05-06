2bd5d4e547804615c4521e22e66c9dad103d1563
Derek <237026504@163.com>
2024-05-06 23:48:37 +0800

    114

--- tree/flatten/114.go
+++ tree/flatten/114.go
@@ -307,3 +307,29 @@ func thirteen(root *TreeNode) {
 		root = root.Right
 	}
 }
+
+func fourteen(root *TreeNode) {
+	if root == nil {
+		return
+	}
+
+	var stack []*TreeNode
+	var list []int
+	head := root
+	for len(stack) > 0 || head != nil {
+		for head != nil {
+			list = append(list, head.Val)
+			stack = append(stack, head)
+			head = head.Left
+		}
+		top := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		head = top.Right
+	}
+
+	for i := 1; i < len(list); i++ {
+		root.Left = nil
+		root.Right = &TreeNode{Val: list[i]}
+		root = root.Right
+	}
+}
