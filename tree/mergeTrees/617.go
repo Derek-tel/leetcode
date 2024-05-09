ae4b04d852138be1b9156f34c26c829afee66ea1
Derek <237026504@163.com>
2024-05-09 23:29:34 +0800

    617

--- tree/mergeTrees/617.go
+++ tree/mergeTrees/617.go
@@ -182,3 +182,16 @@ func thirteen(root1, root2 *TreeNode) *TreeNode {
 	root1.Right = thirteen(root1.Right, root2.Right)
 	return root1
 }
+
+func fourteen(root1, root2 *TreeNode) *TreeNode {
+	if root1 == nil {
+		return root2
+	}
+	if root2 == nil {
+		return root1
+	}
+	root1.Val = root1.Val + root2.Val
+	root1.Left = fourteen(root1.Left, root2.Left)
+	root1.Right = fourteen(root1.Right, root2.Right)
+	return root1
+}
