ccb79557f3f2d5c0f14e738c83b264cc61f1165e
Derek <237026504@163.com>
2024-05-21 23:31:22 +0800

    first equal

--- binary search/searchRange/34.go
+++ binary search/searchRange/34.go
@@ -50,16 +50,16 @@ func last(nums []int, target int) int {
 }
 
 // 二分查找第一个与 target 相等的元素，时间复杂度 O(logn)
-func searchFirstEqualElement(nums []int, target int) int {
+func firstEqual(nums []int, target int) int {
 	low, high := 0, len(nums)-1
 	for low <= high {
-		mid := low + ((high - low) >> 1)
+		mid := low + (high-low)>>1
 		if nums[mid] > target {
 			high = mid - 1
 		} else if nums[mid] < target {
 			low = mid + 1
 		} else {
-			if (mid == 0) || (nums[mid-1] != target) { // 找到第一个与 target 相等的元素
+			if (mid == 0) || nums[mid-1] != target {
 				return mid
 			}
 			high = mid - 1
