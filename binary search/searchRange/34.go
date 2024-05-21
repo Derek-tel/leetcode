e5bcafbe94f0569111d77e1cb0db10d990e03982
Derek <237026504@163.com>
2024-05-21 23:33:04 +0800

    last equal

--- binary search/searchRange/34.go
+++ binary search/searchRange/34.go
@@ -69,16 +69,16 @@ func firstEqual(nums []int, target int) int {
 }
 
 // 二分查找最后一个与 target 相等的元素，时间复杂度 O(logn)
-func searchLastEqualElement(nums []int, target int) int {
+func lastEqual(nums []int, target int) int {
 	low, high := 0, len(nums)-1
 	for low <= high {
-		mid := low + ((high - low) >> 1)
+		mid := low + (high-low)>>1
 		if nums[mid] > target {
 			high = mid - 1
 		} else if nums[mid] < target {
 			low = mid + 1
 		} else {
-			if (mid == len(nums)-1) || (nums[mid+1] != target) { // 找到最后一个与 target 相等的元素
+			if mid == len(nums)-1 || nums[mid+1] != target {
 				return mid
 			}
 			low = mid + 1
