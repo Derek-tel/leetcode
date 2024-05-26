03b0c0d34cf2a68453cbdfc4cfec2097add41d10
Derek <237026504@163.com>
2024-05-26 23:54:18 +0800

    338

--- dp/countBits/338.go
+++ dp/countBits/338.go
@@ -197,6 +197,19 @@ func sixteen(n int) []int {
 	return bit
 }
 
+func seventeen(n int) []int {
+	bit := make([]int, n+1)
+	highBit := 0
+	bit[0] = 0
+	for i := 1; i <= n; i++ {
+		if i&(i-1) == 0 {
+			highBit = i
+		}
+		bit[i] = bit[i-highBit] + 1
+	}
+	return bit
+}
+
 func main() {
 	fmt.Println(countBits(5))
 }
