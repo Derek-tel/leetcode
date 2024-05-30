1ac2b0c6fd398c8e7c4e34b98e15d84405a4ac7f
Derek <237026504@163.com>
2024-05-30 23:58:51 +0800

    62

--- dp/uniquePaths/62.go
+++ dp/uniquePaths/62.go
@@ -256,3 +256,20 @@ func sixteen(m, n int) int {
 	}
 	return dp[m-1][n-1]
 }
+
+func seventeen(m, n int) int {
+	dp := make([][]int, m)
+	for i := 0; i < m; i++ {
+		dp[i] = make([]int, n)
+	}
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			if i == 0 || j == 0 {
+				dp[i][j] = 1
+			} else {
+				dp[i][j] = dp[i-1][j] + dp[i][j-1]
+			}
+		}
+	}
+	return dp[m-1][n-1]
+}
