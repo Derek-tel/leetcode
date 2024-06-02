34095d05c7f0777203e6f02a6845be7dd0dab639
Derek <237026504@163.com>
2024-06-02 23:13:12 +0800

    169

--- hashtable/majorityElement/169.go
+++ hashtable/majorityElement/169.go
@@ -255,3 +255,21 @@ func fourteen(nums []int) int {
 	}
 	return p
 }
+
+func fifteen(nums []int) int {
+	count := 0
+	var p int
+	for _, num := range nums {
+		if count == 0 {
+			p = num
+			count++
+		} else {
+			if p == num {
+				count++
+			} else {
+				count--
+			}
+		}
+	}
+	return p
+}
