3431d26767f5fccf8e16adcbc92a60c337b43462
Derek <237026504@163.com>
2024-06-07 22:50:25 +0800

    160

--- listedlink/getIntersectionNode/160.go
+++ listedlink/getIntersectionNode/160.go
@@ -287,3 +287,23 @@ func fourteen(headA, headB *ListNode) *ListNode {
 	}
 	return pb
 }
+
+func fifteen(headA, headB *ListNode) *ListNode {
+	pa, pb := headA, headB
+	if pa == nil || pb == nil {
+		return nil
+	}
+	for pa != pb {
+		if pa == nil {
+			pa = headB
+		} else {
+			pa = pa.Next
+		}
+		if pb == nil {
+			pb = headA
+		} else {
+			pb = pb.Next
+		}
+	}
+	return pb
+}
